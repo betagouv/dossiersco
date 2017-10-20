75b973cc87141507866fe5bfaf099823cfe258c4
rosedeschamps <puydebois.etienne@gmail.com>
2017-10-20 17:46:27 +0200

    Ajouter une base de données Redis et communiquer avec elle

--- famille/famille.rb
+++ famille/famille.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
+require 'redis'
+
+redis = Redis.new
 
 get '/tableau_de_bord' do
-  erb :tableau_de_bord
+	eleve_keys = redis.keys("ELEVE:*")
+	@liste_eleves = []
+	eleve_keys.each do |eleve_key|
+		@liste_eleves << redis.hgetall(eleve_key)
+	end	
+ 	erb :tableau_de_bord
 end
 
 get '/' do
@@ -19,8 +27,4 @@ end
 post '/eleve' do
 	fiche_eleve = {:prenom => "Etienne", :nom => "Puydebois"}
 	erb :eleve, :locals => fiche_eleve
-end
-
-post '/validation' do
-
 end
