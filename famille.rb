4ebaa32010327ac0edaad7112f09fa211a6f4000
rosedeschamps <puydebois.etienne@gmail.com>
2017-10-30 11:14:43 +0100

    Refonte de l'interface famille

---
+++ famille.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'redis'
+
+redis = Redis.new
+
+get '/tableau_de_bord' do
+	eleve_keys = redis.keys("ELEVE:*")
+	@liste_eleves = []
+	eleve_keys.each do |eleve_key|
+		@liste_eleves << redis.hgetall(eleve_key)
+	end	
+ 	erb :tableau_de_bord
+end
+
+get '/' do
+	erb :identification
+end
+
+post '/accueil' do
+	erb :accueil
+end
+
+post '/refus' do
+	erb :refus
+end
+
+post '/eleve' do
+	fiche_eleve = {:prenom => "Etienne", :nom => "Puydebois"}
+	erb :eleve, :locals => fiche_eleve
+end
