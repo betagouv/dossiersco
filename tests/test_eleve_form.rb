8862ca8479fe4e34f40422fbdb157644b5b3214b
rosedeschamps <puydebois.etienne@gmail.com>
2017-12-15 15:39:53 +0100

    Ajout de tests d'identification et de  modification du lieu de naissance

--- tests/test_eleve_form.rb
+++ tests/test_eleve_form.rb
@@ -14,7 +14,29 @@ class EleveFormTest < Test::Unit::TestCase
 
 	def test_accueil
 		get '/'
-		assert last_response.ok?
 		assert last_response.body.include? 'Inscription'
 	end
+
+	def test_entree_succes
+		post '/accueil', identifiant: '1'
+		assert last_response.body.include? 'Etienne'
+	end
+	
+	def test_entree_echec
+		post '/accueil', identifiant: '3'
+		follow_redirect!
+		assert last_response.body.include? 'aucun élève'
+	end
+
+	def test_modification_lieu_naiss_eleve
+		post '/eleve', ville_naiss: 'Beziers'
+		get '/eleve'
+		assert last_response.body.include? 'Beziers'
+	end
+
+	# def test_mauvaise_connexion
+	# 	get '/eleve?'
+	# 	follow_redirect!
+	# 	assert last_response.body.include? 'Quelques clics'
+	# end
 end
