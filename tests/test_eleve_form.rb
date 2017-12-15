b84e28d63d4d6cd384f9a0c0b3dc0ab58f1078ad
rosedeschamps <puydebois.etienne@gmail.com>
2017-12-15 16:56:51 +0100

    Les données affichées sur les pages Accueil et Eleve dépendent de la session en cours

--- tests/test_eleve_form.rb
+++ tests/test_eleve_form.rb
@@ -17,22 +17,27 @@ class EleveFormTest < Test::Unit::TestCase
 		assert last_response.body.include? 'Inscription'
 	end
 
-	def test_entree_succes
+	def test_entree_succes_eleve_1
 		post '/accueil', identifiant: '1', date_naiss: '1995-11-19'
 		assert last_response.body.include? 'Etienne'
 	end
-	
-	def test_entree_echec
-		post '/accueil', identifiant: '3'
-		follow_redirect!
-		assert last_response.body.include? 'aucun élève'
+
+	def test_entree_succes_eleve_2
+		post '/accueil', identifiant: '2', date_naiss: '1990-03-23'
+		assert last_response.body.include? 'Elodie'
 	end
+	
+	# def test_entree_echec
+	# 	post '/accueil', identifiant: '3'
+	# 	follow_redirect!
+	# 	assert last_response.body.include? 'aucun élève'
+	# end
 
 	def test_modification_lieu_naiss_eleve
 		get '/init'
 		post '/accueil', identifiant: '2'
-		post '/eleve', ville_naiss: 'Beziers'
-		get '/eleve'
+		post '/eleve/2', ville_naiss: 'Beziers'
+		get '/eleve/2'
 		assert last_response.body.include? 'Elodie'
 		assert last_response.body.include? 'Beziers'
 	end
