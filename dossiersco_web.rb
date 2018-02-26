90d03833f5d820f1137ef97cd0ea26af93e5a349
rosedeschamps <puydebois.etienne@gmail.com>
2018-02-26 15:27:04 +0100

    Rajout du parcours réinscription dans le contrôleur

--- dossiersco_web.rb
+++ dossiersco_web.rb
@@ -118,3 +118,33 @@ end
 get '/agent' do
 	erb :'agent', layout: :layout_agent
 end
+
+# REINSCRIPTIONS
+
+get '/r0' do
+	erb :'r0_accueil'
+end
+
+get '/r1' do
+	erb :'r1_scolarite'
+end
+
+get '/r2' do
+	erb :'r2_famille'
+end
+
+get '/r3' do
+	erb :'r3_administration'
+end
+
+get '/r4' do
+	erb :'r4_pieces_a_joindre'
+end
+
+get '/r5' do
+	erb :'r5_validation'
+end
+
+get '/r6' do
+	erb :'r6_confirmation'
+end
