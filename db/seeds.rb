9b4c0cea9e96bce3ee4aee707e6fa151322703c8
Philippe Blayo <philippe.blayo@gmail.com>
2018-03-21 23:59:06 +0100

    Re-initialise la base de donnée entre chaque test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,31 @@
-DossierEleve.destroy_all
-Eleve.destroy_all
+def init
+  DossierEleve.destroy_all
+  Eleve.destroy_all
 
-eleves = [
-    {prenom: 'Edith',
-     nom: 'Piaf',
-     date_naiss: '1915-12-19',
-     nationalite: 'francaise',
-     identifiant: '2'},
-    {prenom: 'Etienne',
-     nom: 'Puydebois',
-     date_naiss: '1995-11-19',
-     nationalite: 'francaise',
-     identifiant: '1'}
-]
+  eleves = [
+      {prenom: 'Edith',
+       nom: 'Piaf',
+       date_naiss: '1915-12-19',
+       nationalite: 'francaise',
+       identifiant: '2'},
+      {prenom: 'Etienne',
+       nom: 'Puydebois',
+       date_naiss: '1995-11-19',
+       nationalite: 'francaise',
+       identifiant: '1'}
+  ]
 
-eleves.each do |u|
-  eleve = Eleve.create!(u)
-  DossierEleve.create!(
-      eleve_id: eleve.id,
-      demarche: "reinscription"
-  )
-  p "eleve #{eleve.id}"
+  eleves_ids = []
+  eleves.each do |u|
+    eleve = Eleve.create!(u)
+    DossierEleve.create!(
+        eleve_id: eleve.id,
+        demarche: "reinscription"
+    )
+    eleves_ids.push eleve.id
+  end
+  p "eleves : #{eleves_ids}"
+
 end
+
+init
