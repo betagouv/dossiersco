e078a6c1a09d23b7dc475c639a97cb22f078846e
Philippe Blayo <philippe.blayo@gmail.com>
2018-03-29 12:29:55 +0200

    Ajoute un identifiant de test

--- helpers/init.rb
+++ helpers/init.rb
@@ -13,8 +13,18 @@ def init
        nom: 'Puydebois',
        date_naiss: '1995-11-19',
        nationalite: 'francaise',
-       identifiant: '1'}
+       identifiant: '1'},
+      {date_naiss: '2005-10-19',
+       nationalite: 'FRANCE',
+       identifiant: '080788316HE',
+       sexe: 'Féminin',
+       ville_naiss: 'PARIS',
+       classe_ant: '5ème',
+       ets_ant: 'College Jean-Francois Oeben',
+       lv2: 'Allemand'
+      }
   ]
+
 
   eleve = Eleve.create!(eleves[0])
   etablissement = Etablissement.create!({
@@ -38,4 +48,33 @@ def init
       demarche: "reinscription"
   )
 
+
+	eleve = Eleve.create!(eleves[2])
+	etablissement = Etablissement.create!({
+    nom: "College Jean-Francois Oeben",
+    date_limite: "samedi 3 juin 2018"
+	})
+	dossier_eleve = DossierEleve.create!(
+			eleve_id: eleve.id,
+			etablissement_id: etablissement.id,
+			demarche: "reinscription"
+	)
+	RespLegal.create!(
+      dossier_eleve_id: dossier_eleve.id,
+      lien_de_parente: 'Mère',
+      situation_emploi: '',
+      profession: '',
+      priorite: 1
+	)
+  RespLegal.create!(
+      dossier_eleve_id: dossier_eleve.id,
+      lien_de_parente: 'Père',
+      email: '',
+      situation_emploi: '',
+      profession: '',
+      priorite: 2
+  )
+  RespLegal.create!(
+      dossier_eleve_id: dossier_eleve.id
+  )
 end
