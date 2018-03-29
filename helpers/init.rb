23d3e90fd4a36a8849c79c86e4b0a579615f50ae
Philippe Blayo <philippe.blayo@gmail.com>
2018-03-29 17:22:04 +0200

    Retire l'élève du dernier test utilisateur

--- helpers/init.rb
+++ helpers/init.rb
@@ -13,18 +13,8 @@ def init
        nom: 'Puydebois',
        date_naiss: '1995-11-19',
        nationalite: 'francaise',
-       identifiant: '1'},
-      {date_naiss: '2005-10-19',
-       nationalite: 'FRANCE',
-       identifiant: '080788316HE',
-       sexe: 'Féminin',
-       ville_naiss: 'PARIS',
-       classe_ant: '5ème',
-       ets_ant: 'College Jean-Francois Oeben',
-       lv2: 'Allemand'
-      }
+       identifiant: '1'}
   ]
-
 
   eleve = Eleve.create!(eleves[0])
   etablissement = Etablissement.create!({
@@ -39,42 +29,12 @@ def init
 
   eleve = Eleve.create!(eleves[1])
   etablissement = Etablissement.create!({
-    nom: "Collège Arago",
-    date_limite: "samedi 3 juin 2018"
-  })
+     nom: "College Jean-Francois Oeben",
+     date_limite: "samedi 3 juin 2018"})
   DossierEleve.create!(
       eleve_id: eleve.id,
       etablissement_id: etablissement.id,
       demarche: "reinscription"
   )
 
-
-	eleve = Eleve.create!(eleves[2])
-	etablissement = Etablissement.create!({
-    nom: "College Jean-Francois Oeben",
-    date_limite: "samedi 3 juin 2018"
-	})
-	dossier_eleve = DossierEleve.create!(
-			eleve_id: eleve.id,
-			etablissement_id: etablissement.id,
-			demarche: "reinscription"
-	)
-	RespLegal.create!(
-      dossier_eleve_id: dossier_eleve.id,
-      lien_de_parente: 'Mère',
-      situation_emploi: '',
-      profession: '',
-      priorite: 1
-	)
-  RespLegal.create!(
-      dossier_eleve_id: dossier_eleve.id,
-      lien_de_parente: 'Père',
-      email: '',
-      situation_emploi: '',
-      profession: '',
-      priorite: 2
-  )
-  RespLegal.create!(
-      dossier_eleve_id: dossier_eleve.id
-  )
 end
