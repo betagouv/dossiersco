ea4cd2dcab52afd713ed4f0fdcad020a8818ff16
Laurent Bossavit <laurent.git@bossavit.com>
2018-03-30 17:02:33 +0200

    Effectuer réellement l'authentification

--- dossiersco_agent.rb
+++ dossiersco_agent.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'bcrypt'
 
 set :database_file, "config/database.yml"
 
@@ -9,12 +10,19 @@ use Rack::Session::Pool
 
 
 get '/agent' do
-	erb :'agent/identification'
+  erb :'agent/identification'
 end
 
 post '/agent' do
-
-  redirect '/tableau_de_bord'
+  agent = Agent.find_by(identifiant: params[:identifiant])
+  mdp_saisi = params[:mot_de_passe]
+  mdp_crypte = BCrypt::Password.new(agent.password)
+  if mdp_crypte == mdp_saisi
+    redirect '/tableau_de_bord'
+  else
+    session[:erreur_login] = "Ces informations ne correspondent pas à un agent enregistré"
+    redirect '/agent'
+  end
 end
 
 get '/tableau_de_bord' do
