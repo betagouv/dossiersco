c10901278b7c58cf9b7cbe771863ee1ddd9b9f9f
lucien mollard <mollard.lucien@gmail.com>
2018-03-30 16:21:38 +0200

    Mise en place de l'authentification agent

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-require_relative '../models/eleve.rb'
-require_relative '../models/dossier_eleve.rb'
-require_relative '../models/resp_legal.rb'
-require_relative '../models/contact_urgence.rb'
-require_relative '../models/etablissement.rb'
-
+require_relative '../helpers/models.rb'
 require_relative '../helpers/init'
 init
