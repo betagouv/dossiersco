185b3a7bd990c06f724beb432e1f3b0e97f0af57
Philippe Blayo <philippe.blayo@gmail.com>
2018-04-26 14:12:39 +0200

    Tente de lire les pièces jointes depuis s3

--- uploaders/fichier_uploader.rb
+++ uploaders/fichier_uploader.rb
@@ -5,8 +5,6 @@ class FichierUploader  < CarrierWave::Uploader::Base
 	def url
 		if ENV['RACK_ENV'] == "test" || !ENV['S3_KEY']
 			"/uploads/"
-		else
-			"https://s3.amazonaws.com/dossierscoweb/dossierscoweb/dossierscoweb/uploads/"
 		end
 	end
 end
