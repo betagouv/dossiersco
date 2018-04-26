7c4f67e2cab1a58b5be9cfd486fa9c533b895f7c
Philippe Blayo <philippe.blayo@gmail.com>
2018-04-26 14:38:35 +0200

    Tente de corriger l'erreur 'hostname does not match the server certificate'

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,32 +8,19 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
 
-	# For testing, upload files to local `tmp` folder.
 	if ENV['RACK_ENV'] == "test" || !ENV['S3_KEY']
 		config.storage = :file
 		config.enable_processing = false
-		#config.root = "#{Rails.root}/tmp"
 	else
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-        # Configuration for Amazon S3 should be made available through an Environment variable.
-        # For local installations, export the env variable through the shell OR
-        # if using Passenger, set an Apache environment variable.
-        #
-        # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
-        #
-        # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
-
-        # Configuration for Amazon S3
         :provider              => 'AWS',
         :aws_access_key_id     => ENV['S3_KEY'],
         :aws_secret_access_key => ENV['S3_SECRET'],
         :region                => ENV['S3_REGION'],
-        :path_style            => true
+        :path_style            => true # indispensable pour éviter l'erreur "hostname does not match the server certificate"
     }
     config.storage = :fog
-
-    #config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
     config.fog_directory    = ENV['S3_BUCKET_NAME']
     config.asset_host       = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
