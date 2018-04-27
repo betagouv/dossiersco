28c3c8100194fc286d985db3ae75e966023b7d29
Philippe Blayo <philippe.blayo@gmail.com>
2018-04-27 13:35:16 +0200

    Corrige l'indentation

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,17 +12,17 @@ CarrierWave.configure do |config|
 		config.storage = :file
 		config.enable_processing = false
 	else
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-        :provider              => 'AWS',
-        :aws_access_key_id     => ENV['S3_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET'],
-        :region                => ENV['S3_REGION'],
-        :path_style            => true # indispensable pour éviter l'erreur "hostname does not match the server certificate"
-    }
-    config.storage = :fog
+        config.fog_provider = 'fog/aws'
+        config.fog_credentials = {
+            :provider              => 'AWS',
+            :aws_access_key_id     => ENV['S3_KEY'],
+            :aws_secret_access_key => ENV['S3_SECRET'],
+            :region                => ENV['S3_REGION'],
+            :path_style            => true # indispensable pour éviter l'erreur "hostname does not match the server certificate"
+        }
+        config.storage = :fog
 
-    config.fog_directory    = ENV['S3_BUCKET_NAME']
-    config.asset_host       = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+        config.fog_directory    = ENV['S3_BUCKET_NAME']
+        config.asset_host       = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
 	end
 end
