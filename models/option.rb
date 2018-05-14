a58dcd69c42b6871b9e734dd877654745f057c84
lucien mollard <mollard.lucien@gmail.com>
2018-05-14 16:52:35 +0200

    permet de créer une option avec un même nom mais un niveau de début différent

--- models/option.rb
+++ models/option.rb
@@ -1,5 +1,5 @@
 class Option < ActiveRecord::Base
   belongs_to :etablissement
-  validates_uniqueness_of :nom, scope: :etablissement_id
+  validates_uniqueness_of :nom, scope: :niveau_debut
   has_and_belongs_to_many :eleve
 end
