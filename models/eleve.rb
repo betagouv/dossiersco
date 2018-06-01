fd937d8ecc445768d6a6b1fd8dff95beda5a6242
Philippe Blayo <philippe.blayo@gmail.com>
2018-06-01 14:54:25 +0200

    [WIP] Génère les groupes d'options obligatoires

--- models/eleve.rb
+++ models/eleve.rb
@@ -2,4 +2,5 @@ class Eleve < ActiveRecord::Base
   has_one :dossier_eleve
   has_and_belongs_to_many :option
   has_many :demande
+  belongs_to :montee
 end
--- models/option.rb
+++ models/option.rb
@@ -8,7 +8,17 @@ class Demande < ActiveRecord::Base
   belongs_to :option
 end
 
+class Demandabilite < ActiveRecord::Base
+  belongs_to :montee
+  belongs_to :option
+end
+
 class Abandon < ActiveRecord::Base
   belongs_to :eleve
+  belongs_to :option
+end
+
+class Abandonnabilite < ActiveRecord::Base
+  belongs_to :montee
   belongs_to :option
 end
