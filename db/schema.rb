040a268c7b8509510c3448717ae56e8ef27b3a2d
lucien mollard <mollard.lucien@gmail.com>
2018-06-07 16:02:29 +0200

    Ajout du statut admin pour changer de collège sur le tableau de bord

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_113026) do
+ActiveRecord::Schema.define(version: 2018_06_07_133616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_06_05_113026) do
     t.integer "etablissement_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean "admin"
   end
 
   create_table "contact_urgences", force: :cascade do |t|
