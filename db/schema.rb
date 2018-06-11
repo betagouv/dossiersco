ccdb9a46503d2729faec52f095949c0abc089757
Laurent Bossavit <laurent.git@bossavit.com>
2018-06-11 15:20:20 +0200

    Ajoute un champ info sur les options

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_120115) do
+ActiveRecord::Schema.define(version: 2018_06_11_130621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2018_06_11_120115) do
     t.string "nom"
     t.string "groupe"
     t.string "modalite"
+    t.string "info"
   end
 
   create_table "piece_attendues", force: :cascade do |t|
--- models/option.rb
+++ models/option.rb
@@ -1,5 +1,8 @@
 class Option < ActiveRecord::Base
   has_and_belongs_to_many :eleve
+  def nom_et_info
+    self.nom + (self.info.present? ? " #{self.info}" : "")
+  end
 end
 
 class Demande < ActiveRecord::Base
