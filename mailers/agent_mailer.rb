9e0b71e8f3b70405a0770b358cf9daa9a6f35187
lucien mollard <mollard.lucien@gmail.com>
2018-06-11 17:17:43 +0200

    Ajoute un champs email pour les collèges et un destinataire pour le reply_to des emails

--- mailers/agent_mailer.rb
+++ mailers/agent_mailer.rb
@@ -2,29 +2,25 @@ class AgentMailer < ActionMailer::Base
     default from: "contact@dossiersco.beta.gouv.fr"
 
     def emails
-        @eleve.dossier_eleve.resp_legal.map{ |resp_legal| resp_legal.email } + ['contact@dossiersco.beta.gouv.fr']
+        @eleve.dossier_eleve.resp_legal.map{ |resp_legal| resp_legal.email } +
+            ['contact@dossiersco.beta.gouv.fr', @eleve.dossier_eleve.etablissement.email]
     end
 
     def contacter_une_famille(eleve, message)
         @message = message
         @eleve = eleve
-        mail(subject: "Réinscription de votre enfant au collège", to: emails) do |format|
-            format.text
-        end
+        mail(subject: "Réinscription de votre enfant au collège",
+            reply_to: ['contact@dossiersco.beta.gouv.fr', @eleve.dossier_eleve.etablissement.email],
+            to: emails) do |format|
+                format.text
+            end
     end
 
     def envoyer_mail_confirmation(eleve)
-        @eleve = eleve
-        mail(subject: "Réinscription de votre enfant au collège", to: emails) do |format|
-            format.text
-        end
+        contacter_une_famille eleve, ''
     end
 
     def mail_validation_inscription(eleve)
-        @eleve = eleve
-        mail(subject: "Réinscription de votre enfant au collège", to: emails) do |format|
-            format.text
-        end
+        contacter_une_famille eleve, ''
     end
-
 end
