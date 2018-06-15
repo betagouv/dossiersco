7d50c9a8902e1d3f2ffbb55b9dc9ae3dcf1c2a60
lucien mollard <mollard.lucien@gmail.com>
2018-06-15 12:39:01 +0200

    Modifie le tableau de bord

--- dossiersco_agent.rb
+++ dossiersco_agent.rb
@@ -56,11 +56,11 @@ end
 
 get '/agent/tableau_de_bord' do
   total_dossiers = agent.etablissement.dossier_eleve.count
-  etats, notes, moyenne, commentaires = stats agent.etablissement
+  etats, notes, moyenne, dossiers_avec_commentaires = stats agent.etablissement
   erb :'agent/tableau_de_bord',
     layout: :layout_agent,
     locals: {agent: agent, total_dossiers: total_dossiers, etats: etats,
-      satisfaction: notes, moyenne: moyenne, commentaires: commentaires}
+      notes: notes, moyenne: moyenne, dossiers_avec_commentaires: dossiers_avec_commentaires}
 end
 
 post '/agent/tableau_de_bord' do
