47736b7798350449d3c865a7c9106bd9b511e349
Philippe Blayo <philippe.blayo@gmail.com>
2018-06-17 22:20:42 +0200

    Met en relief les différences d'adresse

--- models/resp_legal.rb
+++ models/resp_legal.rb
@@ -1,3 +1,12 @@
 class RespLegal < ActiveRecord::Base
   belongs_to :dossier_eleve
+
+  def meme_adresse autre_resp_legal
+    return false if autre_resp_legal.nil?
+    meme_adresse = true
+    ["adresse", "code_postal", "ville"].each do |c|
+      meme_adresse = meme_adresse && (self[c] == autre_resp_legal[c])
+    end
+    meme_adresse
+  end
 end
