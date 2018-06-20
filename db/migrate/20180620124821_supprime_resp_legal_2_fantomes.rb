bc4d1c3d82e64bb6439cf809b92c64d49631b4a3
Philippe Blayo <philippe.blayo@gmail.com>
2018-06-20 15:21:38 +0200

    FIXUP Supprime tout resp legal 2 vide de resp_legals

---
+++ db/migrate/20180620124821_supprime_resp_legal_2_fantomes.rb
@@ -0,0 +1,7 @@
+class SupprimeRespLegal2Fantomes < ActiveRecord::Migration[5.2]
+  def change
+    RespLegal
+      .select {|r| !r.nom.present? && !r.prenom.present? && !r.lien_de_parente.present?}
+      .map(&:destroy!)
+  end
+end
