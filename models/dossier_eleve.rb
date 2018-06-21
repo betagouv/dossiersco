d98cbbfa961dfd4b535af51a09bd89b2570da14d
Laurent Bossavit <laurent.git@bossavit.com>
2018-06-21 16:29:47 +0200

    Envoie des SMS de relance 'derniers jours'

--- models/dossier_eleve.rb
+++ models/dossier_eleve.rb
@@ -1,3 +1,9 @@
+require 'uri'
+require 'net/http'
+require 'net/https'
+require 'json'
+require 'tilt/erb'
+
 class DossierEleve < ActiveRecord::Base
   belongs_to :eleve
   belongs_to :etablissement
@@ -17,4 +23,35 @@ class DossierEleve < ActiveRecord::Base
     enfants = self.resp_legal.first.enfants_a_charge || 0
     enfants > 1
   end
+
+  def relance_sms
+    # Construction du message
+    template = "<%= eleve.dossier_eleve.etablissement.nom %>: attention, derniers jours pour réinscrire votre enfant <%= eleve.prenom %> sur https://dossiersco.fr avec vos identifiants: <%= eleve.identifiant %> et la date de naissance de l'enfant."
+    template = Tilt['erb'].new { template }
+    text = template.render(nil,eleve: eleve)
+
+    rl = resp_legal_1
+    numero = rl.tel_secondaire || rl.tel_principal
+
+    # Avec Nexmo
+    if numero
+      numero_prefixe = numero.gsub(/[[:space:]]/,'').gsub(/^0/,'+33')
+      uri = URI.parse("https://rest.nexmo.com/sms/json")
+
+      https = Net::HTTP.new(uri.host,uri.port)
+      https.use_ssl = true
+
+      header = {'Content-Type': 'application/json'}
+      payload = {
+        'api_key':"#{ENV['NEXMO_KEY']}",
+        'api_secret':"#{ENV['NEXMO_SECRET']}",
+        'from': ENV['NEXMO_SENDER'], 'to': numero_prefixe, 'text':text}
+
+      request = Net::HTTP::Post.new(uri.request_uri, header)
+      request.body = payload.to_json
+
+      response = https.request(request)
+    end
+  end
+
 end
