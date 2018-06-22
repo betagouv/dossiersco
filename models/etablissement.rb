28b24764f69b2734b3fb2becb913f31061792295
Laurent Bossavit <laurent.git@bossavit.com>
2018-06-22 15:00:28 +0200

    Permet d'insérer un message en exploitant un modèle

--- models/etablissement.rb
+++ models/etablissement.rb
@@ -3,6 +3,7 @@ class Etablissement < ActiveRecord::Base
   has_many :agent
   has_many :tache_import
   has_many :piece_attendue
+  has_many :modele
 
   def classes
     dossier_eleve.collect(&:eleve).collect(&:classe_ant).reject(&:nil?).uniq
