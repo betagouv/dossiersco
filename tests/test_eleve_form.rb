ff0e02ff15c40b3f872846962bfc63e1b16826e2
Philippe Blayo <philippe.blayo@gmail.com>
2018-06-22 13:54:22 +0200

    Ajoute note et commentaire dans agent/eleve

--- tests/test_eleve_form.rb
+++ tests/test_eleve_form.rb
@@ -1193,4 +1193,17 @@ class EleveFormTest < Test::Unit::TestCase
     assert RespLegal.new.adresse_inchangee
   end
 
+  def test_un_agent_voit_un_commentaire_parent_dans_vue_eleve
+    e = Eleve.create! identifiant: 'XXX'
+    dossier_eleve = DossierEleve.create! eleve_id: e.id, etablissement_id: Etablissement.first, commentaire: "Commentaire de test"
+    RespLegal.create! dossier_eleve_id: dossier_eleve.id,
+      tel_principal: '0101010101', tel_secondaire: '0606060606', email: 'test@test.com', priorite: 1
+
+    post '/agent', identifiant: 'pierre', mot_de_passe: 'demaulmont'
+    get "/agent/eleve/XXX"
+
+    doc = Nokogiri::HTML(last_response.body)
+    #assert_equal "Commentaire de test", doc.css("div#commentaire").first
+    assert last_response.body.include? 'Commentaire de test'
+  end
 end
