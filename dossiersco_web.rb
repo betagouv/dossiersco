0df47e54dbbcf6b77705041fe6086f61c20eace5
lucien mollard <mollard.lucien@gmail.com>
2018-06-25 16:30:01 +0200

    Factorise l'upload des pj coté controller

--- dossiersco_web.rb
+++ dossiersco_web.rb
@@ -269,21 +269,7 @@ end
 
 post '/enregistre_piece_jointe' do
   dossier_eleve = eleve.dossier_eleve
-  params.each do |code, piece|
-    if params[code].present? and params[code]["tempfile"].present?
-      file = File.open(params[code]["tempfile"])
-      uploader = FichierUploader.new
-      uploader.store!(file)
-      nom_du_fichier = File.basename(file.path)
-      piece_attendue = PieceAttendue.find_by(code: code, etablissement_id: dossier_eleve.etablissement_id)
-      piece_jointe = PieceJointe.find_by(piece_attendue_id: piece_attendue.id, dossier_eleve_id: dossier_eleve.id)
-      if piece_jointe.present?
-        piece_jointe.update(etat: 'soumis', clef: nom_du_fichier)
-      else
-        piece_jointe = PieceJointe.create!(etat: 'soumis', clef: nom_du_fichier, piece_attendue_id: piece_attendue.id, dossier_eleve_id: dossier_eleve.id)
-      end
-    end
-  end
+  upload_pieces_jointes dossier_eleve, params
   redirect '/pieces_a_joindre'
 end
 
