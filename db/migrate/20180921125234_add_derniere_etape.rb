27f964a0065c6a3676befe5353c6e469ff56bfe0
Philippe Blayo <philippe.blayo@gmail.com>
2018-09-21 16:24:37 +0200

    Corrige le retour à la dernière étape visitée

---
+++ db/migrate/20180921125234_add_derniere_etape.rb
@@ -0,0 +1,6 @@
+class AddDerniereEtape < ActiveRecord::Migration[5.2]
+  def change
+    add_column :dossier_eleves, :derniere_etape, :string
+    rename_column :dossier_eleves, :etape, :etape_la_plus_avancee
+  end
+end
