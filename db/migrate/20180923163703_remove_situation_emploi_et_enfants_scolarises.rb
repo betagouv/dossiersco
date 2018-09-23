581d1d44655fa7ddd3fd66653c5c97416e49c263
Philippe Blayo <philippe.blayo@gmail.com>
2018-09-23 23:06:05 +0200

    Retire les colonnes situation & enfants_scolarises

---
+++ db/migrate/20180923163703_remove_situation_emploi_et_enfants_scolarises.rb
@@ -0,0 +1,6 @@
+class RemoveSituationEmploiEtEnfantsScolarises < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :resp_legals, :situation_emploi
+    remove_column :resp_legals, :enfants_a_charge_secondaire 
+  end
+end
