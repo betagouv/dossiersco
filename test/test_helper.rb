8877eae47ea95274dc2bcae37ed39a1a86bc70c3
lucien mollard <mollard.lucien@gmail.com>
2019-01-08 11:11:54 +0100

    affiche une erreur pour une mauvaise date lors de l'identification

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,8 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def html_escape text
+    ERB::Util::html_escape(text)
+  end
   # Add more helper methods to be used by all tests here...
 end
