703f527595e223a3c9aa742e2e3c98b756224da0
lucien mollard <mollard.lucien@gmail.com>
2019-01-09 16:20:06 +0100

    redirige les agents mal identifiés

--- tests/test_agent.rb
+++ tests/test_agent.rb
@@ -25,17 +25,7 @@ class EleveFormTest < Test::Unit::TestCase
     ActionMailer::Base.deliveries = []
   end
 
-  def test_entree_mauvais_mdp_agent
-    post '/agent', identifiant: 'pierre', mot_de_passe: 'pierre'
-    follow_redirect!
-    assert last_response.body.include? 'Ces informations ne correspondent pas à un agent enregistré'
-  end
 
-  def test_entree_mauvais_identifiant_agent
-    post '/agent', identifiant: 'jacques', mot_de_passe: 'pierre'
-    follow_redirect!
-    assert last_response.body.include? 'Ces informations ne correspondent pas à un agent enregistré'
-  end
 
   def test_nombre_dossiers_total
     post '/agent', identifiant: 'pierre', mot_de_passe: 'demaulmont'
