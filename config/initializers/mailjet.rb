629be3fb46cb31b432489138d30dd1a2e644e1f3
lucien mollard <mollard.lucien@gmail.com>
2019-01-10 18:08:54 +0100

    configure Sendgrid

--- config/initializers/mailjet.rb
+++
@@ -1,6 +0,0 @@
-require 'mailjet'
-Mailjet.configure do |config|
-  config.api_key = ENV['MAILJET_API_KEY']
-  config.secret_key = ENV['MAILJET_SECRET_KEY']
-  config.default_from = 'contact@dossiersco.beta.gouv.fr'
-end
