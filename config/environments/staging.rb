ea1373c22788037d0d98f53eae5cc328b9a736f1
lucien mollard <mollard.lucien@gmail.com>
2019-01-15 12:11:38 +0100

    change letter_opener pour letter_opener_web pour être compatible avec heroku et docker
    ajoute aussi letter opener web en environnement de dévelopement

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -65,8 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "app_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :letter_opener_web
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
