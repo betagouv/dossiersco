4885d12c37f1200f468513a147760b0bff01f926
Yannick Francois <yaf@elsif.fr>
2019-01-18 00:10:17 +0100

    test en français également, et migration dans le CI

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    I18n.config.available_locales = :fr
     config.i18n.default_locale = :fr
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
