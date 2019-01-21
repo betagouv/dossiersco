40b1abb7914571341399dca42f9bbbdc6e98467a
Yannick Francois <yaf@elsif.fr>
2019-01-21 23:04:05 +0100

    bloque les emails par défaut
    close #233

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "app_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :mailjet_api
+  if ENV['laisser_partir_les_emails']
+    config.action_mailer.delivery_method = :mailjet_api
+  else
+    config.action_mailer.delivery_method = :letter_opener_web
+  end
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
