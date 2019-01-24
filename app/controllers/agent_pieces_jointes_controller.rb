d37c5ce4453f604165fea170517c723269beba4a
Yannick Francois <yaf@elsif.fr>
2019-01-24 16:51:25 +0100

    permet à un agent d'ajouter des pièces jointes à un dossier élève

---
+++ app/controllers/agent_pieces_jointes_controller.rb
@@ -0,0 +1,25 @@
+class AgentPiecesJointesController < ApplicationController
+  before_action :identification_agent, :retrouve_eleve
+  layout 'layout_agent'
+
+  def create
+    PieceJointe.create!(piece_jointe_params.merge(dossier_eleve: @eleve.dossier_eleve, etat: 'soumis'))
+    redirect_to "/agent/eleve/#{@eleve.identifiant}#dossier"
+  end
+
+  def update
+    piece_jointe = PieceJointe.find(params[:id])
+    piece_jointe.update!(piece_jointe_params.merge(dossier_eleve: @eleve.dossier_eleve, etat: 'soumis'))
+    redirect_to "/agent/eleve/#{@eleve.identifiant}#dossier"
+  end
+
+  private
+  def piece_jointe_params
+    params.require(:piece_jointe).permit(:fichier, :piece_attendue_id)
+  end
+
+  def retrouve_eleve
+    @eleve ||= Eleve.find_by(identifiant: params[:eleve_id])
+  end
+end
+
