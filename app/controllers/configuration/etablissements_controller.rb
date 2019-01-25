979f53f88c66a295048871a0f60a9c652703dc75
Christophe Robillard <geek@robiweb.net>
2019-01-25 20:19:15 +0100

    rend les routes pour les etablissements plus explicites

---
+++ app/controllers/configuration/etablissements_controller.rb
@@ -0,0 +1,28 @@
+module Configuration
+  class EtablissementsController < ApplicationController
+    layout 'configuration'
+
+    before_action :identification_agent
+    before_action :if_agent_is_admin
+
+    def new
+      @etablissement = Etablissement.new
+    end
+
+    def create
+      @etablissement = Etablissement.new(etablissement_params)
+
+      if @etablissement.save
+        redirect_to configuration_path
+      else
+        render :new
+      end
+    end
+
+    private
+    def etablissement_params
+      params.require(:etablissement).permit(:nom, :email, :adresse, :ville, :code_postal, :message_permanence,
+                                            :message_infirmerie, :gere_demi_pension, :signataire)
+    end
+  end
+end
--- test/integration/etablissements_controller_test.rb
+++ test/integration/etablissements_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 require 'fixtures'
 init
 
-class EtablisssementsControllerTest < ActionDispatch::IntegrationTest
+class EtablissementsControllerTest < ActionDispatch::IntegrationTest
   test "Un admin crée un etablissement" do
     admin = Fabricate(:admin)
     identification_agent(admin)
 
-    post etablisssements_path, params: {etablissement: {nom: 'collège de la foresterie'}}
+    post configuration_etablissements_path, params: {etablissement: {nom: 'collège de la foresterie'}}
 
     assert_equal 1, Etablissement.where(nom: 'collège de la foresterie').count
   end
