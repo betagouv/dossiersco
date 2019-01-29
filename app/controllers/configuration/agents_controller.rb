d8e7d4a912264d56646b615e62aee348f46a1f6b
lucien mollard <mollard.lucien@gmail.com>
2019-01-29 11:55:43 +0100

    permet de lister, voir, editer et supprimer des établissements
    close 263
    
    rend plus lisible les listes d'agents, établissement etc dans la partie configuration ainsi que les messages d'erreurs

--- app/controllers/configuration/agents_controller.rb
+++ app/controllers/configuration/agents_controller.rb
@@ -4,13 +4,13 @@ module Configuration
 
     before_action :identification_agent
     before_action :if_agent_is_admin
+    before_action :cherche_agent, only: [:edit, :update, :destroy]
 
     def new
       @agent = Agent.new
     end
 
     def create
-      @agent = Agent.new(agent_params)
       @agent.etablissement ||= agent_connecté.etablissement
 
       if @agent.save
@@ -25,18 +25,25 @@ module Configuration
     end
 
     def edit
-      @agent = Agent.find(params[:id])
     end
 
     def update
-      @agent = Agent.find(params[:id])
       @agent.update(agent_params)
       redirect_to configuration_agents_path
+    end
+
+    def destroy
+      @agent.destroy
+      redirect_to configuration_agents_path, notice: "L'agent a bien été supprimé"
     end
 
     private
     def agent_params
       params.require(:agent).permit(:identifiant, :prenom, :nom, :password, :etablissement_id, :admin)
+    end
+
+    def cherche_agent
+      @agent = Agent.find(params[:id])
     end
   end
 end
