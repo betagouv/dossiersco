d34a4b0ef33d8cd3efbefdc78b295d0aebba37b1
Christophe Robillard <geek@robiweb.net>
2019-01-30 14:31:32 +0100

    devine le département à partir du code postal
    vérifie que le code postal est valide

--- test/models/etablissement_test.rb
+++ test/models/etablissement_test.rb
@@ -2,9 +2,24 @@ require 'test_helper'
 
 class EtablissementTest < ActiveSupport::TestCase
 
-  def test_a_un_fabricant_valide
+  test "a un fabricant valide" do
     assert Fabricate.build(:etablissement).valid?
   end
 
+  test "invalide si le code postal n'a pas 5 chiffres" do
+    assert Fabricate.build(:etablissement, code_postal: 'x').invalid?
+  end
+
+  test "avec un code postal, departement renvoie les 2 premiers chiffres du code postal" do
+    etablissement = Fabricate.build(:etablissement, code_postal: '77400')
+
+    assert '77', etablissement.departement
+  end
+
+  test "sans code postal, departement renvoie une chaine vide" do
+    etablissement = Fabricate.build(:etablissement, code_postal: nil)
+
+    assert '', etablissement.departement
+  end
 end
 
