405bf5f27af2c3e1658c7698c766c5c05cf3972a
Yannick Francois <yaf@elsif.fr>
2019-02-03 22:43:52 +0100

    test l'import d'un petit fichier siecle

--- test/system/configuration_test.rb
+++ test/system/configuration_test.rb
@@ -16,5 +16,30 @@ class ConfigurationTest < ActionDispatch::IntegrationTest
 
     click_link "Configuration"
     click_link "Agents"
+    click_link text: "Créer un nouvel agent"
+
+    attributs = Fabricate.attributes_for(:agent)
+    fill_in "agent_identifiant", with: attributs[:identifiant]
+    fill_in "agent_password", with: attributs[:password]
+    click_button "valider"
+
+    assert_selector "td", text: attributs[:identifiant]
+
+    click_link "Suivi des dossiers"
+    click_link "Import"
+
+    assert_selector "h2", text: "Import depuis siecle"
+
+    attach_file("tache_import_fichier", Rails.root + "test/fixtures/files/test_import_siecle.xls")
+
+    assert_equal 7, DossierEleve.count
+    click_button "Importer le fichier SIECLE"
+    assert_equal 9, DossierEleve.count
+
+    click_link "Configuration"
+    click_link "Options pédagogiques"
+    assert_selector "h1", text: "Options Pedagogiques"
+
+    assert_selector "a", text: "Editer"
   end
 end
