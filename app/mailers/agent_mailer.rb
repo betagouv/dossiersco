034d62d219e2a1854de76501c0c6a83fa34342a7
Christophe Robillard <geek@robiweb.net>
2019-02-06 11:12:59 +0100

    envoie le mail de succès d'import via l'agent mailer

--- app/mailers/agent_mailer.rb
+++ app/mailers/agent_mailer.rb
@@ -50,4 +50,15 @@ class AgentMailer < ApplicationMailer
                 format.text
             end
     end
+
+  def succes_import(etablissement_id, statistiques)
+    @statistiques = statistiques
+    @etablissement = Etablissement.find(etablissement_id)
+    mail(subject: "Import de votre base élève dans DossierSCO",
+         reply_to: 'contact@dossiersco.beta.gouv.fr',
+         delivery_method_options: { api_key: ENV['MAILER_API_KEY'], secret_key: ENV['MAILER_SECRET_KEY'] },
+         to: @etablissement.email) do |format|
+      format.text
+    end
+  end
 end
