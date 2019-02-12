878a80dd303dc7cb735ad3bed9a198331a627665
Christophe Robillard <geek@robiweb.net>
2019-02-12 18:39:13 +0100

    avertit par mail qu'il y a eu un problème lors de l'import
    close #342

--- app/jobs/traiter_imports_job.rb
+++ app/jobs/traiter_imports_job.rb
@@ -4,9 +4,13 @@ class TraiterImportsJob < ApplicationJob
   queue_as :default
 
   def perform(tache_id, email)
-    tache = TacheImport.find(tache_id)
-    statistiques = import_xls tache.fichier.path, tache.etablissement_id
-    mail = AgentMailer.succes_import(email, statistiques)
-    mail.deliver_now
+    begin
+      tache = TacheImport.find(tache_id)
+      statistiques = import_xls tache.fichier.path, tache.etablissement_id
+      mail = AgentMailer.succes_import(email, statistiques)
+      mail.deliver_now
+    rescue
+      AgentMailer.erreur_import(email).deliver_now
+    end
   end
 end
