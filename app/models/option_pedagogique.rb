1809e239e05c2e472695d2a94393c6e03729c955
Christophe Robillard <geek@robiweb.net>
2019-02-18 11:43:15 +0100

    purge uniquement les dossiers d'inscription

--- app/models/option_pedagogique.rb
+++ app/models/option_pedagogique.rb
@@ -1,4 +1,5 @@
 class OptionPedagogique < ApplicationRecord
+  belongs_to :etablissement
   has_and_belongs_to_many :mef
   has_and_belongs_to_many :dossier_eleves
 
--- test/fabricators/option_pedagogique_fabricator.rb
+++ test/fabricators/option_pedagogique_fabricator.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 Fabricator(:option_pedagogique) do
+  etablissement
 end
