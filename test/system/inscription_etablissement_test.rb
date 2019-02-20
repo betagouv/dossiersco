7e66d6ddadb0955ee2b0809469d5bbfe30fece8a
Yannick Francois <yaf@elsif.fr>
2019-02-21 00:04:54 +0100

    ajoute un test sur l'inscription établissement

---
+++ test/system/inscription_etablissement_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class InscriptionEtablissementTest < ActionDispatch::IntegrationTest
+
+  test 'Inscription simple' do
+    visit '/'
+    click_link 'Vous êtes agent en EPLE ?'
+    click_link 'Inscrire mon établissement sur DossierSCO'
+    fill_in 'Uai', with: '0780119F'
+    click_button 'Inscrire l\'établissement'
+    assert_selector 'div', text: 'Un mail a été envoyé à ce.0780119F@ac-paris.fr'
+
+    etablissement = Etablissement.find_by(uai: '0780119F')
+    assert_equal '0780119F', etablissement.uai
+
+    agent = Agent.find_by(etablissement: etablissement)
+    visit "/configuration/agents/#{agent.id}/activation?jeton=#{agent.jeton}"
+
+    assert_selector 'h1', text: 'Activation du compte ce.0780119F@ac-paris.fr'
+    assert_selector 'h1', text: 'Activation du compte ce.0780119F@ac-yvelines.fr'
+  end
+
+end
