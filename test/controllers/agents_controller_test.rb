910c8a314409f16d8962043e847529c0ee8951f4
lucien mollard <mollard.lucien@gmail.com>
2019-02-21 16:12:12 +0100

    ajoute un agent administrateur

--- test/controllers/agents_controller_test.rb
+++ test/controllers/agents_controller_test.rb
@@ -5,13 +5,12 @@ require 'test_helper'
 class AgentsControllerTest < ActionDispatch::IntegrationTest
   test 'Un admin crée un agent qui a un établissement lié' do
     admin = Fabricate(:admin)
-    etablissement = Fabricate(:etablissement)
+    etablissement = admin.etablissement
     identification_agent(admin)
 
-    post configuration_agents_path, params: { agent: { identifiant: 'identifiant quelconque', password: 'password quelconque',
-                                                       etablissement_id: etablissement.id, email: 'test@test.fr' } }
+    post configuration_agents_path, params: { agent: { email: 'test@test.fr' } }
 
-    assert_equal 1, Agent.where(identifiant: 'identifiant quelconque', etablissement_id: etablissement.id).count
+    assert_equal 1, Agent.where(email: 'test@test.fr', etablissement_id: etablissement.id).count
   end
 
   test 'Un admin liste les agents de son établiseement' do
