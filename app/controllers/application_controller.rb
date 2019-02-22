7a7faf779aa4fae6031728d3d54059dee8ed9789
Yannick Francois <yaf@elsif.fr>
2019-02-22 15:59:21 +0100

    Redirige à l'accueil agent si pas connecté
    avant d'écrire des traces ou d'envoyer des infos pour sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,16 +18,18 @@ class ApplicationController < ActionController::Base
   end
 
   def identification_agent
-    identifiant = agent_connecté.present? ? agent_connecté.identifiant : '<anonyme>'
+    unless agent_connecté.present?
+      redirect_to '/agent'
+      return
+    end
     ajoute_information_utilisateur_pour_sentry({
       type_utilisateur: "agent",
       dossiersco_id: agent_connecté.id
     })
-    Trace.create(identifiant: identifiant,
+    Trace.create(identifiant: agent_connecté.identifiant,
                  categorie: 'agent',
                  page_demandee: request.path_info,
                  adresse_ip: request.ip)
-    redirect_to '/agent' unless agent_connecté.present?
   end
 
   def if_agent_is_admin
