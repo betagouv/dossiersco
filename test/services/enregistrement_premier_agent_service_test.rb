fd36c422ba91525b7c23e73e5ebe4dd5036181ca
Yannick Francois <yaf@elsif.fr>
2019-03-04 12:08:26 +0100

    empêche l'enregistrement si l'uai n'est pas trouvé

--- test/services/enregistrement_premier_agent_service_test.rb
+++ test/services/enregistrement_premier_agent_service_test.rb
@@ -12,6 +12,12 @@ class EnregistrementPremierAgentServiceTest < ActiveSupport::TestCase
     end
   end
 
+  test "pas de création d'établissement si l'uai n'est pas valide" do
+    assert_emails 0 do
+      assert ! EnregistrementPremierAgentService.new.execute('0753936y')
+    end
+  end
+
   test 'avec un UAI du 78, l\'adresse est celle de l\'académie de Versailles' do
       service = EnregistrementPremierAgentService.new
       email = service.construit_email_chef_etablissement('0780119F')
@@ -26,47 +32,52 @@ class EnregistrementPremierAgentServiceTest < ActiveSupport::TestCase
 
   test "l'uai 0753936w est valide" do
     service = EnregistrementPremierAgentService.new
-    assert service.uai_valid?('0753936w')
+    assert service.uai_valide?('0753936w')
+  end
+
+  test "l'uai 0572015F, avec une clef en majuscule, est valide" do
+    service = EnregistrementPremierAgentService.new
+    assert service.uai_valide?('0572015F')
   end
 
   test "l'uai 753936w n'est pas à la longueur 8" do
     service = EnregistrementPremierAgentService.new
-    assert ! service.uai_valid?('753936w')
+    assert ! service.uai_valide?('753936w')
   end
 
   test "l'uai 753936w n'a pas le bon format, le dernier caractère n'est pas une lettre clef" do
     service = EnregistrementPremierAgentService.new
-    assert ! service.uai_valid?('07539369')
+    assert ! service.uai_valide?('07539369')
   end
 
   test "la clef 'i' est interdite dans les uai" do
     service = EnregistrementPremierAgentService.new
-    assert ! service.uai_valid?('0753936i')
+    assert ! service.uai_valide?('0753936i')
   end
 
   test "la clef 'o' est interdite dans les uai" do
     service = EnregistrementPremierAgentService.new
-    assert ! service.uai_valid?('0753936o')
+    assert ! service.uai_valide?('0753936o')
   end
 
   test "la clef 'q' est interdite dans les uai" do
     service = EnregistrementPremierAgentService.new
-    assert ! service.uai_valid?('0753936q')
+    assert ! service.uai_valide?('0753936q')
   end
 
   test "la clef 'p' n'est pas la bonne clef pour cet uai" do
     service = EnregistrementPremierAgentService.new
-    assert ! service.uai_valid?('0753936p')
+    assert ! service.uai_valide?('0753936p')
   end
 
   test "l'uai de la corse du sud a comme département 620" do
     service = EnregistrementPremierAgentService.new
-    assert ! service.uai_valid?('6200006M')
+    assert service.uai_valide?('6200006M')
   end
 
   test "l'uai de la haute corse a comme département 720" do
     service = EnregistrementPremierAgentService.new
-    assert ! service.uai_valid?('7200727C')
+    assert service.uai_valide?('7200727C')
   end
 
 end
