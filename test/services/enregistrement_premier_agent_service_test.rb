26ddd1cc7561aaeb1772980803889372e2cf156f
Yannick Francois <yaf@elsif.fr>
2019-03-04 11:40:00 +0100

    permet à tout les établissements de france de s'inscrire sur dossiersco
    close #406
    
    1) Description
    Le répertoire national des établissements (RNE) est une base de données
    dans laquelle chaque établissement scolaire de l'Éducation nationale
    française est enregistré et se voit attribuer un code alphanumérique dit
    code RNE.Depuis 1996, le code UAI (Unité Administrative Immatriculée)
    remplace le code RNE en reprenant sa nomenclature. Toutefois, la
    dénomination code RNE reste majoritairement utilisée.
    
    Ce code est composé de 7 chiffres suivis d'une lettre. Les trois
    premiers chiffres indiquent le département, la lettre est une clé de
    vérification de la saisie. La clé, issue d'un alphabet français de 23
    lettres excluant les lettres I, O et Q, est la lettre dont la position,
    comprise entre 0 et 22, correspond au reste de la division des 7
    premiers chiffres par 23.
    
    2) Test
    Guillaume APOLLINAIRE à Thiais : 0940123 Y
    940123 / 23 = 940102. Reste 21 = (0940123 - 094102). Y est la 21ème
    lettre d'un alphabet excluant I, O et Q (attention A=zéro, B=1…)

--- test/services/enregistrement_premier_agent_service_test.rb
+++ test/services/enregistrement_premier_agent_service_test.rb
@@ -23,4 +23,50 @@ class EnregistrementPremierAgentServiceTest < ActiveSupport::TestCase
       email = service.construit_email_chef_etablissement('0753936w')
       assert_equal 'ce.0753936w@ac-paris.fr', email
   end
+
+  test "l'uai 0753936w est valide" do
+    service = EnregistrementPremierAgentService.new
+    assert service.uai_valid?('0753936w')
+  end
+
+  test "l'uai 753936w n'est pas à la longueur 8" do
+    service = EnregistrementPremierAgentService.new
+    assert ! service.uai_valid?('753936w')
+  end
+
+  test "l'uai 753936w n'a pas le bon format, le dernier caractère n'est pas une lettre clef" do
+    service = EnregistrementPremierAgentService.new
+    assert ! service.uai_valid?('07539369')
+  end
+
+  test "la clef 'i' est interdite dans les uai" do
+    service = EnregistrementPremierAgentService.new
+    assert ! service.uai_valid?('0753936i')
+  end
+
+  test "la clef 'o' est interdite dans les uai" do
+    service = EnregistrementPremierAgentService.new
+    assert ! service.uai_valid?('0753936o')
+  end
+
+  test "la clef 'q' est interdite dans les uai" do
+    service = EnregistrementPremierAgentService.new
+    assert ! service.uai_valid?('0753936q')
+  end
+
+  test "la clef 'p' n'est pas la bonne clef pour cet uai" do
+    service = EnregistrementPremierAgentService.new
+    assert ! service.uai_valid?('0753936p')
+  end
+
+  test "l'uai de la corse du sud a comme département 620" do
+    service = EnregistrementPremierAgentService.new
+    assert ! service.uai_valid?('6200006M')
+  end
+
+  test "l'uai de la haute corse a comme département 720" do
+    service = EnregistrementPremierAgentService.new
+    assert ! service.uai_valid?('7200727C')
+  end
+
 end
