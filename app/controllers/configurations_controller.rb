a0b36a8be65a0d782f9df55e26d8b0c7f6343b65
Yannick Francois <yaf@elsif.fr>
2019-03-11 12:16:12 +0100

    affiche des boites d'information dans le tableau de bord de configuration

--- app/controllers/configurations_controller.rb
+++ app/controllers/configurations_controller.rb
@@ -5,6 +5,9 @@ class ConfigurationsController < ApplicationController
   before_action :if_agent_is_admin
 
   def show
+    stats = Struct.new(:agents, :options, :mef, :dossiers, :date_fin, :uai, :nom_etablissement, :code_postal)
+    @stats = stats.new
+    @stats.agents = Agent.where(etablissement: @agent_connecté.etablissement).count
   end
 
 end
