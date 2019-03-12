9be967322825a3658da79db9db3b04e284bc972a
lucien mollard <mollard.lucien@gmail.com>
2019-03-12 17:25:07 +0100

    initie le changement de template pour celui de data-gouv

---
+++ app/controllers/configuration/pieces_attendues_controller.rb
@@ -0,0 +1,52 @@
+module Configuration
+  class PiecesAttenduesController < ApplicationController
+    layout 'configuration'
+
+    before_action :identification_agent
+    before_action :set_piece_attendue, only: [:edit, :update, :destroy]
+
+    def index
+      @pieces_attendues = PieceAttendue.where(etablissement: agent_connecté.etablissement)
+    end
+
+    def new
+      @piece_attendue = PieceAttendue.new
+    end
+
+    def edit
+    end
+
+    def create
+      @piece_attendue = PieceAttendue.new(piece_attendue_params)
+      @piece_attendue.etablissement = agent_connecté.etablissement
+
+      if @piece_attendue.save
+        redirect_to configuration_pieces_attendue_index_url, notice: 'Piece attendue was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @piece_attendue.update(piece_attendue_params)
+        redirect_to configuration_mef_index_url, notice: 'Piece attendue was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @piece_attendue.destroy
+      redirect_to configuration_mef_index_url, notice: 'Piece attendue was successfully destroyed.'
+    end
+
+    private
+    def set_piece_attendue
+      @piece_attendue = PieceAttendue.find(params[:id])
+    end
+
+    def piece_attendue_params
+      params.require(:piece_attendue).permit(:nom, :code, :etablissement_id, :explication, :obligatoire)
+    end
+  end
+end
