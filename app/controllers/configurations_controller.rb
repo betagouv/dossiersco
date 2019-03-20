b3f894bf9a4811b2e635bc85e49dffa58d34948c
lucien mollard <mollard.lucien@gmail.com>
2019-03-20 13:40:35 +0100

    affiche le nombre d'élèves sans mef sur le tableau de bord configuration close #319

--- app/controllers/configurations_controller.rb
+++ app/controllers/configurations_controller.rb
@@ -6,11 +6,13 @@ class ConfigurationsController < ApplicationController
 
   def show
     etablissement = @agent_connecté.etablissement
-    stats = Struct.new(:agents, :options, :mef, :dossiers, :date_fin, :uai, :nom_etablissement, :code_postal, :pieces_attendues)
+    stats = Struct.new(:agents, :options, :mef, :dossiers, :date_fin, :uai, :nom_etablissement, :code_postal,
+                       :pieces_attendues, :eleves_sans_mef)
     @stats = stats.new
     @stats.agents = Agent.where(etablissement: etablissement).count
     @stats.options = etablissement.options_pedagogiques.count
     @stats.mef = etablissement.mef.count
+    @stats.eleves_sans_mef = etablissement.dossier_eleve.where(mef_origine: nil).count
     @stats.dossiers = etablissement.dossier_eleve.count
     @stats.date_fin = etablissement.date_limite
     @stats.uai = etablissement.uai
@@ -18,5 +20,4 @@ class ConfigurationsController < ApplicationController
     @stats.code_postal = etablissement.code_postal
     @stats.pieces_attendues = etablissement.pieces_attendues.map(&:nom)
   end
-
 end
