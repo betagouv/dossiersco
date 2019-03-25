fc0d739a9b4536c7eea4e88573b8971dc668aad2
lucien mollard <mollard.lucien@gmail.com>
2019-03-25 15:29:30 +0100

    upload les fichiers sur S3

--- app/uploaders/fichier_etablissement_uploader.rb
+++ app/uploaders/fichier_etablissement_uploader.rb
@@ -4,7 +4,9 @@ class FichierEtablissementUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
- # storage :fog
+  if Rails.env.production?
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
