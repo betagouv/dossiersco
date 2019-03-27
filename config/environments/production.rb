a9db42797c0b9027a5f41fc77b910163738c4c1a
lucien mollard <mollard.lucien@gmail.com>
2019-03-27 15:52:45 +0100

    enleve des slashs dans l'url dans action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {
-    host: "https://dossiersco.fr/"
+    host: "https://dossiersco.fr"
   }
 
   if ENV['laisser_partir_les_emails']
