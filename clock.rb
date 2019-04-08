3ffa21c47b70fa12ee6f32a61088316e690fe4cd
Yannick Francois <yaf@elsif.fr>
2019-04-08 23:39:13 +0200

    premier test de job régulier

--- clock.rb
+++ clock.rb
@@ -11,10 +11,14 @@ module Clockwork
   #   puts "Running #{job}, at #{time}"
   # end
 
-  every(10.seconds, 'frequent.job')
-  every(3.minutes, 'less.frequent.job')
-  every(1.hour, 'hourly.job')
 
-  every(1.day, 'midnight.job', :at => '00:00')
+  every(1.minutes, 'reload demo db', :at => '00:30', :if => lambda { |t| Rails.env.demo?  }) do
+    puts 'reload demo ?'
+  end
+  #every(10.seconds, 'frequent.job')
+  #every(3.minutes, 'less.frequent.job')
+  #every(1.hour, 'hourly.job')
+
+  #every(1.day, 'midnight.job', :at => '00:00')
 end
 
