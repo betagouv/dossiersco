258f51addab5b2fdb2337a56e98f0066b44fcd62
Yannick Francois <yaf@elsif.fr>
2019-04-09 17:20:04 +0200

    corrige quelques points de tests sur l'export xml siecle

--- test/fabricators/dossier_eleve_fabricator.rb
+++ test/fabricators/dossier_eleve_fabricator.rb
@@ -3,4 +3,6 @@
 Fabricator(:dossier_eleve) do
   eleve
   etablissement
+  mef_destination { Fabricate(:mef) }
+  mef_origine { Fabricate(:mef) }
 end
--- test/fabricators/etablissement_fabricator.rb
+++ test/fabricators/etablissement_fabricator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Fabricator(:etablissement) do
-  uai { sequence(:uai) { |i| "UAI-#{i}" } }
+  uai { sequence(:uai) { |i| "000000#{i}X" } }
   nom { sequence(:nom) { |i| "NOM-#{i}" } }
 end
