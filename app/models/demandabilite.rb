118a3ff7fdfe92957aafd021aaa8b2f410ce0aaa
lucien mollard <lucien.mollard@beta.gouv.fr>
2019-04-16 10:35:04 +0200

    supprime les models inutilisés

--- app/models/demandabilite.rb
+++
@@ -1,4 +0,0 @@
-class Demandabilite < ActiveRecord::Base
-  belongs_to :montee
-  belongs_to :option
-end
--- app/models/montee.rb
+++
@@ -1,5 +0,0 @@
-class Montee < ActiveRecord::Base
-   has_many :demandabilite
-   has_many :abandonnabilite
-   belongs_to :etablissement
-end
