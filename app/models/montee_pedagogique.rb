9ff191e69ccc5f62ab0fad0f4b466e8420400486
lucien mollard <lucien.mollard@beta.gouv.fr>
2019-04-17 11:46:04 +0200

    résout des conflits liés au rebase

--- app/models/montee_pedagogique.rb
+++ app/models/montee_pedagogique.rb
@@ -2,22 +2,14 @@ class MonteePedagogique < ActiveRecord::Base
   belongs_to :mef_origine, class_name: 'Mef'
   belongs_to :mef_destination, class_name: 'Mef'
   belongs_to :option_pedagogique
-<<<<<<< HEAD
   belongs_to :etablissement
-=======
->>>>>>> permet d'ajouter des montées pédagogiques
 
   validate :unique
 
   def unique
-<<<<<<< HEAD
     montee = MonteePedagogique.find_by(mef_destination: self.mef_destination, mef_origine: self.mef_origine,
                                        option_pedagogique: self.option_pedagogique, etablissement_id: self.etablissement_id)
     if montee.present? && montee.id != self.id
-=======
-    montee = MonteePedagogique.find_by(mef_destination: self.mef_destination, mef_origine: self.mef_origine, option_pedagogique: self.option_pedagogique)
-    if montee.present?
->>>>>>> permet d'ajouter des montées pédagogiques
       errors.add(:montee_pedagogique, "existe déjà")
     end
   end
