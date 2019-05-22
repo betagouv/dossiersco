e31632dbc96ce33a0f815ff80da1976d92ebb7dd
lucien mollard <mollard.lucien@gmail.com>
2019-05-22 14:51:07 +0200

    corrige un bug d'ajout des pièces jointes à un élève coté agent

--- app/controllers/agent_pieces_jointes_controller.rb
+++ app/controllers/agent_pieces_jointes_controller.rb
@@ -8,19 +8,19 @@ class AgentPiecesJointesController < ApplicationController
 
   def create
     PieceJointe.create!(piece_jointe_params.merge(dossier_eleve: @eleve.dossier_eleve, etat: "soumis"))
-    redirect_to "/agent/eleve/#{@eleve.identifiant}#dossier"
+    redirect_to "/agent/eleve/#{@eleve.identifiant}"
   end
 
   def update
     piece_jointe = PieceJointe.find(params[:id])
     piece_jointe.update!(piece_jointe_params.merge(dossier_eleve: @eleve.dossier_eleve, etat: "soumis"))
-    redirect_to "/agent/eleve/#{@eleve.identifiant}#dossier"
+    redirect_to "/agent/eleve/#{@eleve.identifiant}"
   end
 
   private
 
   def piece_jointe_params
-    params.require(:piece_jointe).permit(:fichier, :piece_attendue_id)
+    params.require(:piece_jointe).permit({ fichiers: [] }, :piece_attendue_id)
   end
 
   def eleve
