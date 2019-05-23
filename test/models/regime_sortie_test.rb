6bda9aa9776b3bd113b7cf627e05ae55dea61f9a
Yannick Francois <yannick.francois@beta.gouv.fr>
2019-05-23 18:20:12 +0200

    empeche de supprimer un régime de sortie quand au moins un dossier l'a choisi

--- test/models/regime_sortie_test.rb
+++ test/models/regime_sortie_test.rb
@@ -8,4 +8,21 @@ class RegimeSortieTest < ActiveSupport::TestCase
     assert Fabricate.build(:regime_sortie).valid?
   end
 
+  test "un regime de sortie ne peut pas être supprimer si un dossier_eleve à selectionné ce régime" do
+    regime = Fabricate(:regime_sortie)
+    Fabricate(:dossier_eleve, regime_sortie: regime)
+
+    assert_raise Exception do
+      regime.destroy
+    end
+  end
+
+  test "un regime de sortie connais le nombre de dossier lié" do
+    regime = Fabricate(:regime_sortie)
+    3.times { Fabricate(:dossier_eleve, regime_sortie: regime) }
+    Fabricate(:dossier_eleve)
+
+    assert_equal 3, regime.dossier_eleves.count
+  end
+
 end
