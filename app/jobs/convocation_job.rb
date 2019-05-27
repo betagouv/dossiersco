c8344e17297c73b6eeb0bbb41043bc5ae33e9418
Yannick Francois <yannick.francois@beta.gouv.fr>
2019-05-27 16:27:12 +0200

    supprime l'envoie de mail pour les convocations

--- app/jobs/convocation_job.rb
+++ app/jobs/convocation_job.rb
@@ -5,9 +5,6 @@ class ConvocationJob < ActiveJob::Base
   def perform(etablissement, agent)
     pdf = GenerePdf.new
     fichier_a_telecharger = pdf.generer_pdf_par_classes(etablissement, "PdfConvocation")
-
-    mailer = AgentMailer.convocation(agent, fichier_a_telecharger)
-    mailer.deliver_now
   end
 
 end
