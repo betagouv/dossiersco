d461bcffce291820c36f268f4dde28f635c18a3b
Yannick Francois <yannick.francois@beta.gouv.fr>
2019-05-29 10:55:30 +0200

    change l'email par défault pour mailjet

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -3,5 +3,5 @@
 Mailjet.configure do |config|
   config.api_key = ENV["MAILER_API_KEY"]
   config.secret_key = ENV["MAILER_SECRET_KEY"]
-  config.default_from = "contact@dossiersco.beta.gouv.fr"
+  config.default_from = "equipe@dossiersco.fr"
 end
