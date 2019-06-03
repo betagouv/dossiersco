8e40b8cc19d4182c2156543a798b0c37f81e251c
Yannick Francois <yannick.francois@beta.gouv.fr>
2019-06-03 10:45:04 +0200

    redirige la confirmation sur l'accueil

--- config/routes.rb
+++ config/routes.rb
@@ -88,6 +88,7 @@ Rails.application.routes.draw do
 
   get "/accueil", to: "accueil#accueil"
   post "/accueil", to: "accueil#post_accueil"
+  get "/confirmation", to: "accueil#accueil"
 
   get "/eleve", to: "accueil#eleve"
   post "/eleve", to: "accueil#post_eleve"
