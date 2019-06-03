259be0a810e67c8dadde63d2490ed5745ffdc7cb
lucien mollard <mollard.lucien@gmail.com>
2019-06-03 18:44:13 +0200

    corrige les tests sur la récupération des options

--- test/services/presenter_options_pedagogiques_test.rb
+++ test/services/presenter_options_pedagogiques_test.rb
@@ -11,38 +11,16 @@ class PresenterOptionsPedagogiquesTest < ActiveSupport::TestCase
   end
 
   test "on récupere une option ouverte à l'inscription" do
-    option = Fabricate(:option_pedagogique)
-    mef = Fabricate(:mef, options_pedagogiques: [option])
-    dossier_eleve = Fabricate(:dossier_eleve, mef_destination: mef)
-    options = PresenterOptionsPedagogiques.new(dossier_eleve).options
+    mef_option = Fabricate(:mef_option_pedagogique, ouverte_inscription: true)
+    options = PresenterOptionsPedagogiques.new(mef_option.mef).options
 
-    assert_equal [option], options
+    assert_equal [mef_option.option_pedagogique], options
   end
 
   test "on ne récupere pas une option fermée à l'inscription" do
     mef_option = Fabricate(:mef_option_pedagogique, ouverte_inscription: false)
-    dossier_eleve = Fabricate(:dossier_eleve, mef_destination: mef_option.mef)
 
-    assert_equal [], PresenterOptionsPedagogiques.new(dossier_eleve).options
-  end
-
-  test "l'option est abandonnable et que l'élève l'avait l'année passée" do
-    option = Fabricate(:option_pedagogique)
-    mef = Fabricate(:mef, options_pedagogiques: [option])
-    dossier_eleve = Fabricate(:dossier_eleve, mef_destination: mef)
-    dossier_eleve.options_origines = { option.id.to_s => { "nom" => option.nom } }
-    options = PresenterOptionsPedagogiques.new(dossier_eleve).options
-
-    assert_equal [option], options
-  end
-
-  test "l'option est non abandonnable et que l'élève l'avait l'année passée" do
-    mef_option = Fabricate(:mef_option_pedagogique, abandonnable: false)
-    dossier_eleve = Fabricate(:dossier_eleve, mef_destination: mef_option.mef)
-    dossier_eleve.options_origines = { mef_option.option_pedagogique.id.to_s => { "nom" => mef_option.option_pedagogique.nom } }
-    options = PresenterOptionsPedagogiques.new(dossier_eleve).options
-
-    assert_equal [mef_option.option_pedagogique], options
+    assert_equal [], PresenterOptionsPedagogiques.new(mef_option.mef).options
   end
 
 end
