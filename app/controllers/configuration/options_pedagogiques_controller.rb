3b5f4ef24b722243182f27ef79c932289913ce73
lucien mollard <mollard.lucien@gmail.com>
2019-06-05 13:11:42 +0200

    sauvegarde l'explication d'une option

--- app/controllers/configuration/options_pedagogiques_controller.rb
+++ app/controllers/configuration/options_pedagogiques_controller.rb
@@ -85,7 +85,8 @@ module Configuration
     end
 
     def option_pedagogique_params
-      params.require(:option_pedagogique).permit(:libelle, :code_matiere, :nom, :obligatoire, :groupe, mef_ids: [])
+      params.require(:option_pedagogique).permit(:libelle, :code_matiere, :nom, :obligatoire, :groupe,
+                                                 :explication, mef_ids: [])
     end
 
   end
