c6befe5032fdfd7e91a51f57179fb6adfe81adb7
Yannick Francois <yannick.francois@beta.gouv.fr>
2019-06-13 17:11:20 +0200

    réduis la taille des blocks de code

--- app/services/pdf_convocation.rb
+++ app/services/pdf_convocation.rb
@@ -15,63 +15,15 @@ class PdfConvocation
         pdf.default_leading 3
         pdf.move_down 20
 
-        pdf.text_box "#{etablissement.ville}, le #{Time.now.strftime('%d/%m/%Y')}", at: [0, pdf.cursor],
-                                                                                    width: pdf.bounds.width, align: :right
-
-        pdf.text "DossierSCO", size: 20
-
-        pdf.text "Collège #{etablissement.nom}", leading: 5, style: :bold
-        pdf.text "#{etablissement.adresse} #{etablissement.code_postal} #{etablissement.ville}", style: :bold
-
+        affiche_entete(pdf, etablissement)
         pdf.move_down 20
-        pdf.text "Inscriptions #{Time.now.strftime('%Y')}/#{Time.now.strftime('%Y').to_i + 1}", size: 20, align: :center
 
-        pdf.move_down 20
-        pdf.text "Madame, Monsieur,"
-
-        pdf.move_down 10
-        pdf.text "Votre enfant <b>#{dossier_eleve.eleve.prenom} #{dossier_eleve.eleve.nom}</b> est affecté(e) au " \
-          " Collège #{etablissement.nom}, #{etablissement.ville}.", inline_format: true,
-                                                                    indent_paragraphs: 20
-        pdf.text "Pour procéder à son inscription pour la prochaine année scolaire, nous vous invitons à " \
-          "vous connecter, sur un ordinateur de bureau, ordinateur portable, tablette connectée ou " \
-          "téléphone connecté, à l’adresse ci-dessous :", inline_format: true
-
-        pdf.move_down 10
-        pdf.text "https://dossiersco.fr", size: 16, align: :center
-
-        pdf.move_down 10
-        pdf.text "(Pour garantir l’accès le plus rapide au site, merci de saisir cette adresse dans la barre " \
-          "d’adresse de votre navigateur en saisissant l’adresse en entier, et non dans la barre " \
-          "de recherche)", inline_format: true, indent_paragraphs: 20
-        pdf.text "Veuillez vous munir des pièces suivantes ou de leurs photocopies :"
-
+        affiche_texte(pdf, etablissement, dossier_eleve.eleve)
         pdf.move_down 5
 
-        if etablissement.pieces_attendues.count > 7
-          hauteur_debut_colonne = pdf.cursor
-
-          pdf.bounding_box([0, hauteur_debut_colonne], width: (pdf.bounds.width / 2)) do
-            etablissement.pieces_attendues[0...7].each do |piece|
-              pdf.text "- #{piece.nom}", indent_paragraphs: 20
-            end
-          end
-
-          pdf.bounding_box([(pdf.bounds.width / 2), hauteur_debut_colonne],
-                           width: (pdf.bounds.width / 2),
-                           height: 115) do
-                             etablissement.pieces_attendues[7...14].each do |piece|
-                               pdf.text "- #{piece.nom}", indent_paragraphs: 20
-                             end
-                           end
-
-        else
-          etablissement.pieces_attendues.each do |piece|
-            pdf.text "- #{piece.nom}"
-          end
-        end
-
+        affiche_pieces_attendues(pdf, etablissement)
         pdf.move_down 10
+
         pdf.text "Pour le bon déroulement de l’inscription en ligne, merci de vous connecter "\
           "et de vérifier les informations vous concernant avant le : #{etablissement.date_limite}", inline_format: true, indent_paragraphs: 20
 
@@ -88,6 +40,65 @@ class PdfConvocation
           "pour toute question relative à l’inscription.", size: 9, at: [0, 40], width: 530
 
         pdf.start_new_page if nombre_de_dossier < dossiers_eleve.length
+      end
+    end
+  end
+
+  def affiche_entete(pdf, etablissement)
+    pdf.text_box "#{etablissement.ville}, le #{Time.now.strftime('%d/%m/%Y')}", at: [0, pdf.cursor],
+                                                                                width: pdf.bounds.width, align: :right
+
+    pdf.text "DossierSCO", size: 20
+
+    pdf.text "Collège #{etablissement.nom}", leading: 5, style: :bold
+    pdf.text "#{etablissement.adresse} #{etablissement.code_postal} #{etablissement.ville}", style: :bold
+
+    pdf.move_down 20
+    pdf.text "Inscriptions #{Time.now.strftime('%Y')}/#{Time.now.strftime('%Y').to_i + 1}", size: 20, align: :center
+  end
+
+  def affiche_texte(pdf, etablissement, eleve)
+    pdf.text "Madame, Monsieur,"
+
+    pdf.move_down 10
+    pdf.text "Votre enfant <b>#{eleve.prenom} #{eleve.nom}</b> est affecté(e) au " \
+      " Collège #{etablissement.nom}, #{etablissement.ville}.", inline_format: true,
+                                                                indent_paragraphs: 20
+    pdf.text "Pour procéder à son inscription pour la prochaine année scolaire, nous vous invitons à " \
+      "vous connecter, sur un ordinateur de bureau, ordinateur portable, tablette connectée ou " \
+      "téléphone connecté, à l’adresse ci-dessous :", inline_format: true
+
+    pdf.move_down 10
+    pdf.text "https://dossiersco.fr", size: 16, align: :center
+
+    pdf.move_down 10
+    pdf.text "(Pour garantir l’accès le plus rapide au site, merci de saisir cette adresse dans la barre " \
+      "d’adresse de votre navigateur en saisissant l’adresse en entier, et non dans la barre " \
+      "de recherche)", inline_format: true, indent_paragraphs: 20
+    pdf.text "Veuillez vous munir des pièces suivantes ou de leurs photocopies :"
+  end
+
+  def affiche_pieces_attendues(pdf, etablissement)
+    if etablissement.pieces_attendues.count > 7
+      hauteur_debut_colonne = pdf.cursor
+
+      pdf.bounding_box([0, hauteur_debut_colonne], width: (pdf.bounds.width / 2)) do
+        etablissement.pieces_attendues[0...7].each do |piece|
+          pdf.text "- #{piece.nom}", indent_paragraphs: 20
+        end
+      end
+
+      pdf.bounding_box([(pdf.bounds.width / 2), hauteur_debut_colonne],
+                       width: (pdf.bounds.width / 2),
+                       height: 115) do
+                         etablissement.pieces_attendues[7...14].each do |piece|
+                           pdf.text "- #{piece.nom}", indent_paragraphs: 20
+                         end
+                       end
+
+    else
+      etablissement.pieces_attendues.each do |piece|
+        pdf.text "- #{piece.nom}"
       end
     end
   end
--- app/services/pdf_fiche_infirmerie.rb
+++ app/services/pdf_fiche_infirmerie.rb
@@ -4,10 +4,11 @@ class PdfFicheInfirmerie
 
   attr_accessor :nom
 
+  CHAMP_LIBRE = "..............................."
+
   def initialize(etablissement, classe_des_eleves, dossiers_eleve)
     self.nom = "#{classe_des_eleves}.pdf"
     dossier = "tmp/#{etablissement.id}"
-    champ_libre = "..............................."
     FileUtils.mkdir_p(dossier) unless File.directory?(dossier)
     Prawn::Document.generate("#{dossier}/#{nom}") do |pdf|
       nombre_de_dossier = 0
@@ -15,88 +16,19 @@ class PdfFicheInfirmerie
         nombre_de_dossier += 1
         pdf.default_leading 7
         pdf.move_down 20
-        pdf.text "Collège #{etablissement.nom}", leading: 0, size: 10
-        pdf.text etablissement.adresse.to_s, leading: 0, size: 10
-        pdf.text "#{etablissement.code_postal} #{etablissement.ville}", leading: 0, size: 10
 
-        pdf.bounding_box([280, 700], width: 250, height: 50) do
-          pdf.text "Année #{Time.now.strftime('%Y')}-#{Time.now.strftime('%Y').to_i + 1}", size: 14, align: :right
-        end
-        pdf.text "Fiche infirmerie", size: 20, align: :center
+        affiche_etablissement(pdf, etablissement)
+        affiche_entete(pdf)
         pdf.move_down 15
 
-        pdf.text "<b>NOM</b> : #{dossier_eleve.eleve.nom},    <b>Prénom</b> : #{dossier_eleve.eleve.prenom}," \
-                 "    <b>Classe</b> : #{champ_libre}", inline_format: true
-        pdf.text "Date de naissance : #{dossier_eleve.eleve.date_naiss.to_date.strftime('%d/%m/%Y')}" \
-                 "  Scolarité antérieure :" \
-                 " #{dossier_eleve.eleve.classe_ant.present? ? dossier_eleve.eleve.classe_ant : champ_libre}"
+        affiche_eleve(pdf, dossier_eleve.eleve)
         pdf.move_down 8
 
-        dossier_eleve.resp_legal.each do |responsable|
-          pdf.text "<b>Responsable légal #{responsable.priorite}</b>" \
-                   "  Nom : #{responsable.nom}  Prénom : #{responsable.prenom}", inline_format: true
-          pdf.text "Lien avec l'élève : #{responsable.lien_de_parente}"
-          pdf.text "Adresse : #{responsable.adresse}, #{responsable.code_postal} #{responsable.ville}"
-          pdf.text "Tel. Personnel : #{telelephone(:personnel)} ," \
-                   "    Portable : #{responsable.tel_portable.present? ? responsable.tel_portable : champ_libre}," \
-                   "     Pro : #{responsable.tel_professionnel.present? ? responsable.tel_professionnel : champ_libre}"
-        end
+        affiche_les_respresentant_legaux(pdf, dossier_eleve.resp_legal)
+        affiche_la_personne_en_cas_urgence(pdf, dossier_eleve.contact_urgence)
         pdf.move_down 8
 
-        if dossier_eleve.resp_legal.length < 2
-          pdf.text "<b>Responsable légal 2</b>  Nom : #{champ_libre}  Prénom : #{champ_libre}", inline_format: true
-          pdf.text "Lien avec l'élève : #{champ_libre}"
-          pdf.text "Adresse : #{champ_libre}#{champ_libre}#{champ_libre}"
-          pdf.text "Tel. Personnel : #{champ_libre},     Portable : #{champ_libre},     Pro : #{champ_libre}"
-          pdf.move_down 8
-        end
-
-        pdf.text "Numéro de sécurité sociale du responsable : #{champ_libre}"
-        pdf.move_down 8
-
-        pdf.text "Personne à prévenir en cas d'absence des responsables légaux :"
-        if dossier_eleve.contact_urgence.nil?
-          pdf.text "Nom : #{champ_libre}  Prénom : #{champ_libre}", inline_format: true
-          pdf.text "Lien avec l'élève : #{champ_libre}"
-          pdf.text "Tel. Personnel : #{champ_libre},     Portable : #{champ_libre}"
-        else
-          pdf.text "Nom : #{dossier_eleve.contact_urgence.nom}" \
-                   "  Prénom : #{dossier_eleve.contact_urgence.prenom}", inline_format: true
-          pdf.text "Lien avec l'élève : #{dossier_eleve.contact_urgence.lien_avec_eleve}"
-
-          pdf.text "Tel. Principal :" \
-                   " #{telephone(:principal)}," \
-                   "     Secondaire : "\
-                   " #{telephone(:secondaire)}"
-        end
-        pdf.move_down 8
-
-        pdf.text "Assurance scolaire, nom de la compagnie #{champ_libre}, N° de police #{champ_libre}"
-
-        pdf.text "En cas d'urgence, un élève accidenté ou malade est orienté et transporté, par les services de" \
-                 " secours, vers l'hôpital le plus proche. La famille est immédiatement avertie par nos soins." \
-                 " Un mineur ne peut sortir de l'hôpital qu'accompagné d'un responsable légal; En cas de" \
-                 " transport par ambulance privée, les frais sont pris en charge par l'assurance maladie de" \
-                 " la famille.", leading: 0, align: :justify, size: 10
-        pdf.text "Observations particulières que vous jugez utiles de porter à connaissance du service médical" \
-                 " (allergie, maladie chronique, traitement en cours, précautions" \
-                 " particulières...)", leading: 0, size: 10
-        pdf.move_down 8
-        pdf.text ".................................................................................................." \
-                 ".............................................................................................." \
-                 ".............................................................................................." \
-                 ".............................................................................................." \
-                 ".............................................................................................." \
-                 "........."
-
-        pdf.text "Si votre enfant bénéficie d'un PAI (projet d'accueil individualisé) pour des troubles de santé" \
-                 " chroniques, des allergies..., d'un PPS (plan personnalisé de scolarité) ou d'un PAP (plan" \
-                 " d'accompagnement personnalisé) pour des troubles des apprentissage, veuillez prendre contact" \
-                 " avec l'infirmier(ère) de l'établissement ou la/le médecin" \
-                 " scolaire.", leading: 0, size: 10, align: :justify
-        pdf.move_down 8
-        pdf.text "Date et signature du responsable légal :", size: 10
-
+        affiche_bas_de_page(pdf)
         pdf.start_new_page if nombre_de_dossier < dossiers_eleve.length
       end
     end
@@ -106,8 +38,101 @@ class PdfFicheInfirmerie
     if responsable.send("tel_#{type}").present?
       responsable.send("tel_#{type}")
     else
-      champ_libre
+      CHAMP_LIBRE
     end
+  end
+
+  def affiche_les_respresentant_legaux(pdf, representants)
+    representants.each do |responsable|
+      pdf.text "<b>Responsable légal #{responsable.priorite}</b>" \
+        "  Nom : #{responsable.nom}  Prénom : #{responsable.prenom}", inline_format: true
+      pdf.text "Lien avec l'élève : #{responsable.lien_de_parente}"
+      pdf.text "Adresse : #{responsable.adresse}, #{responsable.code_postal} #{responsable.ville}"
+      pdf.text "Tel. Personnel : #{telelephone(:personnel)} ," \
+        "    Portable : #{responsable.tel_portable.present? ? responsable.tel_portable : CHAMP_LIBRE}," \
+        "     Pro : #{responsable.tel_professionnel.present? ? responsable.tel_professionnel : CHAMP_LIBRE}"
+    end
+
+    pdf.move_down 8
+
+    if representants.length < 2
+      pdf.text "<b>Responsable légal 2</b>  Nom : #{CHAMP_LIBRE}  Prénom : #{CHAMP_LIBRE}", inline_format: true
+      pdf.text "Lien avec l'élève : #{CHAMP_LIBRE}"
+      pdf.text "Adresse : #{CHAMP_LIBRE}#{CHAMP_LIBRE}#{CHAMP_LIBRE}"
+      pdf.text "Tel. Personnel : #{CHAMP_LIBRE},     Portable : #{CHAMP_LIBRE},     Pro : #{CHAMP_LIBRE}"
+      pdf.move_down 8
+    end
+
+    pdf.text "Numéro de sécurité sociale du responsable : #{CHAMP_LIBRE}"
+    pdf.move_down 8
+  end
+
+  def affiche_etablissement(pdf, etablissement)
+    pdf.text "Collège #{etablissement.nom}", leading: 0, size: 10
+    pdf.text etablissement.adresse.to_s, leading: 0, size: 10
+    pdf.text "#{etablissement.code_postal} #{etablissement.ville}", leading: 0, size: 10
+  end
+
+  def affiche_la_personne_en_cas_urgence(pdf, contact_urgence)
+    pdf.text "Personne à prévenir en cas d'absence des responsables légaux :"
+    if contact_urgence.nil?
+      pdf.text "Nom : #{CHAMP_LIBRE}  Prénom : #{CHAMP_LIBRE}", inline_format: true
+      pdf.text "Lien avec l'élève : #{CHAMP_LIBRE}"
+      pdf.text "Tel. Personnel : #{CHAMP_LIBRE},     Portable : #{CHAMP_LIBRE}"
+    else
+      pdf.text "Nom : #{contact_urgence.nom}" \
+        "  Prénom : #{contact_urgence.prenom}", inline_format: true
+      pdf.text "Lien avec l'élève : #{contact_urgence.lien_avec_eleve}"
+
+      pdf.text "Tel. Principal :" \
+        " #{telephone(:principal)}," \
+        "     Secondaire : "\
+        " #{telephone(:secondaire)}"
+    end
+  end
+
+  def affiche_entete(pdf)
+    pdf.bounding_box([280, 700], width: 250, height: 50) do
+      pdf.text "Année #{Time.now.strftime('%Y')}-#{Time.now.strftime('%Y').to_i + 1}", size: 14, align: :right
+    end
+
+    pdf.text "Fiche infirmerie", size: 20, align: :center
+  end
+
+  def affiche_eleve(pdf, eleve)
+    pdf.text "<b>NOM</b> : #{eleve.nom},    <b>Prénom</b> : #{eleve.prenom}," \
+      "    <b>Classe</b> : #{CHAMP_LIBRE}", inline_format: true
+    pdf.text "Date de naissance : #{eleve.date_naiss.to_date.strftime('%d/%m/%Y')}" \
+      "  Scolarité antérieure :" \
+      " #{eleve.classe_ant.present? ? eleve.classe_ant : CHAMP_LIBRE}"
+  end
+
+  def affiche_bas_de_page(pdf)
+    pdf.text "Assurance scolaire, nom de la compagnie #{CHAMP_LIBRE}, N° de police #{CHAMP_LIBRE}"
+
+    pdf.text "En cas d'urgence, un élève accidenté ou malade est orienté et transporté, par les services de" \
+      " secours, vers l'hôpital le plus proche. La famille est immédiatement avertie par nos soins." \
+      " Un mineur ne peut sortir de l'hôpital qu'accompagné d'un responsable légal; En cas de" \
+      " transport par ambulance privée, les frais sont pris en charge par l'assurance maladie de" \
+      " la famille.", leading: 0, align: :justify, size: 10
+    pdf.text "Observations particulières que vous jugez utiles de porter à connaissance du service médical" \
+      " (allergie, maladie chronique, traitement en cours, précautions" \
+      " particulières...)", leading: 0, size: 10
+    pdf.move_down 8
+    pdf.text ".................................................................................................." \
+      ".............................................................................................." \
+      ".............................................................................................." \
+      ".............................................................................................." \
+      ".............................................................................................." \
+      "........."
+
+    pdf.text "Si votre enfant bénéficie d'un PAI (projet d'accueil individualisé) pour des troubles de santé" \
+      " chroniques, des allergies..., d'un PPS (plan personnalisé de scolarité) ou d'un PAP (plan" \
+      " d'accompagnement personnalisé) pour des troubles des apprentissage, veuillez prendre contact" \
+      " avec l'infirmier(ère) de l'établissement ou la/le médecin" \
+      " scolaire.", leading: 0, size: 10, align: :justify
+    pdf.move_down 8
+    pdf.text "Date et signature du responsable légal :", size: 10
   end
 
 end
