5b53438e790ca7526b55461b159571aaf4c2d3ea
Philippe Blayo <philippe.blayo@gmail.com>
2019-06-21 20:43:58 +0200

    remonte l'adresse dans l'export siecle

--- test/controllers/configuration/exports_controller_test.rb
+++ test/controllers/configuration/exports_controller_test.rb
@@ -57,4 +57,46 @@ class ExportsControllerTest < ActionDispatch::IntegrationTest
     assert_equal [], xsd.validate(xml)
   end
 
+  test "L'adresse est renseignée dans l'export siècle" do
+    admin = Fabricate(:admin)
+    identification_agent(admin)
+
+    resp = Fabricate(:resp_legal,
+                     adresse: "3 rue de test",
+                     code_postal: "75000",
+                     ville: "Ville de test",
+                     pays: "FRA")
+    Fabricate(:dossier_eleve,
+              etablissement: admin.etablissement,
+              resp_legal: [resp])
+    %i[adresse code_postal ville pays].each do |partie_du_tag_adresse|
+      resp_incomplet = Fabricate(:resp_legal,
+                                 adresse: "adresse à ne pas afficher",
+                                 code_postal: "75001",
+                                 ville: "Ville à ne pas afficher",
+                                 pays: "XXX")
+      resp_incomplet[partie_du_tag_adresse] = nil
+      Fabricate(:dossier_eleve,
+                etablissement: admin.etablissement,
+                resp_legal: [resp_incomplet])
+    end
+
+    get export_siecle_configuration_exports_path
+
+    assert_response :success
+
+    schema = Rails.root.join("doc/import_prive/schema_Import_3.1.xsd")
+    xsd = Nokogiri::XML::Schema(File.read(schema))
+    xml = Nokogiri::XML(response.body)
+    assert_equal [], xsd.validate(xml)
+    assert_match "3 rue de test", response.body
+    assert_match "75000", response.body
+    assert_match "Ville de test", response.body
+    assert_match "FRA", response.body
+    assert_no_match "adresse à ne pas afficher", response.body
+    assert_no_match "75001", response.body
+    assert_no_match "Ville à ne pas afficher", response.body
+    assert_no_match "XXX", response.body
+  end
+
 end
