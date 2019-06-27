1a4806aabe2c72a69f0ec49f6c8a95c82bff66cc
Philippe Blayo <philippe.blayo@gmail.com>
2019-06-27 23:26:29 +0200

    se conformer au style de rubocop

--- script_test_siecle.rb
+++ script_test_siecle.rb
@@ -6,13 +6,11 @@ fichier = ARGV[0]
 puts fichier
 
 if fichier
-  if fichier !~ /[[:digit:]]{7}[[:upper:]]PRIVE[[:digit:]]{14}/ then
+  unless /[[:digit:]]{7}[[:upper:]]PRIVE[[:digit:]]{14}/.match?(fichier)
     puts "Le nom du fichier est du type 0750680GPRIVE2018190626160300.xml (UAI puis PRIVE puis la date du jour et l'heure)"
   end
   aujourdhui = Time.now.strftime("%y%m%d")
-  if !fichier.include? aujourdhui then
-    puts "Le nom du fichier doit contenir la date du jour : #{aujourdhui}"
-  end
+  puts "Le nom du fichier doit contenir la date du jour : #{aujourdhui}" unless fichier.include? aujourdhui
 
   schema = "./doc/import_prive/schema_Import_3.1.xsd"
   xsd = Nokogiri::XML::Schema(File.read(schema))
