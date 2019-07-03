4e8f8f7caa17f02444bf4a0168ca550a2a204d3a
lucien mollard <mollard.lucien@gmail.com>
2019-07-03 17:30:12 +0200

    rechange le chemin du fichier (cette fois c'est la bonne)

--- app/jobs/export_pieces_jointes_job.rb
+++ app/jobs/export_pieces_jointes_job.rb
@@ -25,7 +25,7 @@ class ExportPiecesJointesJob < ActiveJob::Base
               begin
                 zipfile.add(
                   "#{mef.libelle}/#{eleve_folder}/#{fichier.model.piece_attendue.nom}-#{index}.#{format}",
-                  File.join("#{Rails.root.join('public')}#{fichier.url.gsub('%20', ' ').gsub('%C3%A9', 'é').gsub('%C3%89', 'É')}")
+                  File.join(fichier.current_path)
                 )
               rescue StandardError
                 next
