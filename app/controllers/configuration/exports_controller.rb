02a24f45336d17ee47cfa398bd9391b6b5bd0911
lucien mollard <mollard.lucien@gmail.com>
2019-07-09 15:36:52 +0200

    Générer un fichier xml au bon nom dans un zip #671

--- app/controllers/configuration/exports_controller.rb
+++ app/controllers/configuration/exports_controller.rb
@@ -22,10 +22,20 @@ module Configuration
                   end
 
       xml_string = render_to_string layout: false
-      file = Tempfile.new(["export_pour_siecle", ".xml"])
+      nom_fichier = "#{@agent_connecte.etablissement.uai}PRIVE#{Time.now.strftime('%Y')}#{Time.now.strftime('%y').to_i + 1}#{Time.now.strftime('%m%d%I%M%S')}"
+      file = Tempfile.new("#{nom_fichier}.xml")
       file.write(xml_string)
       file.close
-      send_file file.path, x_sendfile: true, type: "text/xml"
+
+      nom_zip = "#{nom_fichier}.zip"
+      temp_file = Tempfile.new(nom_zip)
+
+      Zip::File.open(temp_file.path, Zip::File::CREATE) do |zipfile|
+        zipfile.add("#{nom_fichier}.xml", file)
+      end
+      zip_data = File.read(temp_file.path)
+      send_data(zip_data, :type => 'application/zip', filename: nom_zip)
+
     end
 
     def export_pieces_jointes
