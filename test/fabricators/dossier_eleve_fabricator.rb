5ad7cd2dcf380d743dec10c690d6078b4bf34dfc
Yannick Francois <yannick.francois@beta.gouv.fr>
2019-08-09 12:39:37 +0200

    exporte vers siecle les infos divisions

--- test/fabricators/dossier_eleve_fabricator.rb
+++ test/fabricators/dossier_eleve_fabricator.rb
@@ -5,4 +5,7 @@ Fabricator(:dossier_eleve) do
   etablissement
   mef_destination { |attrs| Fabricate(:mef, etablissement: attrs[:etablissement]) }
   mef_origine { |attrs| Fabricate(:mef, etablissement: attrs[:etablissement]) }
+  mef_an_dernier "12345678901"
+  division_an_dernier "4 D"
+  division "3 A"
 end
