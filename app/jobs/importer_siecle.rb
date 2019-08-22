f842c20dc3c7ab479c752c9cbdf7a909e099a8d2
Yannick Francois <yannick.francois@beta.gouv.fr>
2019-08-22 18:41:43 +0200

    prend en compte des fichiers zip, xml et Excel
    On regarde le type du fichier pour déduire s'il faut dézipper, importer,
    ou passer par une analyse excel

--- app/jobs/importer_siecle.rb
+++ app/jobs/importer_siecle.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "zip"
+
 class ImporterSiecle < ApplicationJob
 
   queue_as :default
@@ -12,26 +14,69 @@ class ImporterSiecle < ApplicationJob
     tache = TacheImport.find(tache_id)
     tache.update(statut: TacheImport::STATUTS[:en_traitement])
 
-    if tache.import_nomenclature?
-      ImportNomenclature.new.perform(tache)
-      mail = AgentMailer.succes_import_nomenclature(email)
-    elsif tache.import_responsables?
-      ImportResponsable.new.perform(tache)
-      mail = AgentMailer.succes_import_responsables(email)
-    elsif tache.import_eleves?
-      ImportEleves.new.perform(tache)
-      mail = AgentMailer.succes_import_eleves(email)
+    file = "#{Rails.root}/public/#{tache.fichier}"
+
+    if file_type_zip?(file)
+      Zip::File.open(file) do |zipfile|
+        zipfile.each do |file_entry|
+          path = file_entry.zipfile.split("/")[0..-2].join("/")
+          declenche_import("#{path}/#{file_entry}", tache, email)
+        end
+      end
     else
+      declenche_import(file, tache, email)
+    end
+
+    tache.update(statut: TacheImport::STATUTS[:terminee])
+  rescue StandardError => e
+    logger.debug e
+    tache.update(statut: TacheImport::STATUTS[:en_erreur])
+    AgentMailer.erreur_import(email).deliver_now
+  end
+
+  def declenche_import(file, tache, email)
+    if file_type_xml?(file)
+      mail = send("import_fichier_#{tache.type_fichier}", tache, email)
+      mail.deliver_now
+    elsif file_type_excel?(file)
       importeur = ImportEleveComplete.new
       importeur.perform(tache)
       mail = AgentMailer.succes_import(email, importeur.statistiques)
+      mail.deliver_now
+    else
+      raise StandardError, "type de fichier non reconnu"
     end
-    mail.deliver_now
+  end
 
-    tache.update(statut: TacheImport::STATUTS[:terminee])
-  rescue StandardError
-    tache.update(statut: TacheImport::STATUTS[:en_erreur])
-    AgentMailer.erreur_import(email).deliver_now
+  def file_type_excel?(file)
+    file_type(file) == "application/vnd.ms-excel"
+  end
+
+  def file_type_xml?(file)
+    file_type(file) == "text/xml"
+  end
+
+  def file_type_zip?(file)
+    file_type(file) == "application/zip"
+  end
+
+  def file_type(file)
+    `file -ib #{file}`.delete("\n").split(";")[0]
+  end
+
+  def import_fichier_nomenclature(tache, email)
+    ImportNomenclature.new.perform(tache)
+    AgentMailer.succes_import_nomenclature(email)
+  end
+
+  def import_fichier_responsables(tache, email)
+    ImportResponsables.new.perform(tache)
+    AgentMailer.succes_import_responsables(email)
+  end
+
+  def import_fichier_eleves(tache, email)
+    ImportEleves.new.perform(tache)
+    AgentMailer.succes_import_eleves(email)
   end
 
 end
--- test/jobs/importer_siecle_test.rb
+++ test/jobs/importer_siecle_test.rb
@@ -23,4 +23,22 @@ class ImporterSiecleTest < ActiveJob::TestCase
     assert_equal "terminée", tache.statut
   end
 
+  test "arrive à lire un zip" do
+    fichier_zip = fixture_file_upload("files/nomenclature_simple.zip")
+    tache = Fabricate(:tache_import, fichier: fichier_zip, type_fichier: "nomenclature")
+    assert_equal "en attente", tache.statut
+    ImporterSiecle.perform_now(tache.id, "an_email@example.com")
+    tache.reload
+    assert_equal "terminée", tache.statut
+  end
+
+  test "arrive à lire un non zippé" do
+    fichier = fixture_file_upload("files/nomenclature_simple.xml")
+    tache = Fabricate(:tache_import, fichier: fichier, type_fichier: "nomenclature")
+    assert_equal "en attente", tache.statut
+    ImporterSiecle.perform_now(tache.id, "an_email@example.com")
+    tache.reload
+    assert_equal "terminée", tache.statut
+  end
+
 end
