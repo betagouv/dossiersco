5ce1e385521967a15834c95b338873aefe0edb58
Yannick Francois <yannick.francois@beta.gouv.fr>
2019-08-26 11:50:50 +0200

    enrichi les code options incompletes

---
+++ scripts/ajout_code_gestion_au_options_origines.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+etablissement = Etablissement.find_by(uai: '0752550P')
+
+dossiers = etablissement.dossier_eleve
+
+dossiers.sample(1).each do |dossier|
+  puts "dossier.options_origines : #{dossier.options_origines}"
+  dossier.options_origines.keys.each do |option_id|
+    puts "key : #{option_id}"
+    option = OptionPedagogique.find(option_id)
+
+    option_origine = {}
+    option_origine[:nom] = option.nom
+    option_origine[:code_matiere] = option.code_matiere
+    option_origine[:groupe] = option.groupe
+
+    puts option_origine
+    dossier.options_origines[option.id] = option_origine
+  end
+  dossier.save!
+end
