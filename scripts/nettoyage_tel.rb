169e684504f31e1eda69ac0d81cae9000575b594
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2019-08-28 19:17:40 +0200

    corrige le script de nettoyage

--- scripts/nettoyage_tel.rb
+++ scripts/nettoyage_tel.rb
@@ -1,10 +1,6 @@
-LONGUEUR_TEL=10
-puts "Nettoie les téléphones..."
-["tel_personnel", "tel_portable", "tel_professionnel"].each do |type_tel|
-  ['6','7'].each{|prefixe| corrige_tel(type_tel, prefixe)}
-  supprime_tel
-end
+# frozen_string_literal: true
 
+LONGUEUR_TEL = 10
 def corrige_tel(type_tel, prefixe)
   RespLegal.where("length(#{type_tel}) = #{LONGUEUR_TEL - 1}").where("#{type_tel} LIKE ?", "#{prefixe}%").each do |r|
     puts "Corrige #{r[type_tel.to_sym]}"
@@ -13,8 +9,15 @@ def corrige_tel(type_tel, prefixe)
 end
 
 def supprime_tel(type_tel)
-  params = Hash.new
+  params = {}
   params[type_tel.to_sym] = nil
   nb_tel_supprimes = RespLegal.where("length(#{type_tel}) < #{LONGUEUR_TEL} AND length(#{type_tel}) > 0").update_all(params)
   puts "#{nb_tel_supprimes} #{type_tel} supprimés"
 end
+
+puts "Nettoie les téléphones..."
+%w[tel_personnel tel_portable tel_professionnel].each do |type_tel|
+  %w[6 7].each { |prefixe| corrige_tel(type_tel, prefixe) }
+  supprime_tel(type_tel)
+end
+
