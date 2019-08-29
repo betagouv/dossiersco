3983b536f9bfc278bbaa8e6c7562e4a3ba1527a6
Yannick Francois <yannick.francois@beta.gouv.fr>
2019-08-29 17:50:35 +0200

    Renome une method pour que ça parle plus
    Co-authored-by: krichtof <christophe.robillard@beta.gouv.fr>

--- scripts/nettoyage_tel.rb
+++ scripts/nettoyage_tel.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 LONGUEUR_TEL = 10
-def corrige_tel(type_tel, prefixe)
+
+def complete_tel_portable(type_tel, prefixe)
   RespLegal.where("length(#{type_tel}) = #{LONGUEUR_TEL - 1}").where("#{type_tel} LIKE ?", "#{prefixe}%").each do |r|
     puts "Corrige #{r[type_tel.to_sym]}"
     r.update_attribute(type_tel.to_sym, "0#{r[type_tel.to_sym]}")
@@ -17,6 +18,8 @@ end
 
 puts "Nettoie les téléphones..."
 %w[tel_personnel tel_portable tel_professionnel].each do |type_tel|
-  %w[6 7].each { |prefixe| corrige_tel(type_tel, prefixe) }
+  %w[6 7].each { |prefixe| complete_tel_portable(type_tel, prefixe) }
   supprime_tel(type_tel)
 end
+
+
