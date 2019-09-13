5d32b954cd32fc87281d448241bf4761c6e36f55
Yannick Francois <yannick.francois@beta.gouv.fr>
2019-09-13 23:03:15 +0200

    correction orthographe et rubocop

--- scripts/met_a_jour_mef_destination_pour_debussy.rb
+++ scripts/met_a_jour_mef_destination_pour_debussy.rb
@@ -8,18 +8,19 @@ xml = Nokogiri::XML(file)
 xml.xpath("//ELEVE").each do |noeud_eleve|
   ine = noeud_eleve.xpath("ID_NATIONAL").text
   next if ine.blank?
+
   dossier = DossierEleve.joins(:eleve).where("eleves.identifiant = ?", ine).first
   next if dossier.blank?
+
   cm2 = Mef.find_by(etablissement: etablissement, libelle: "CM2")
   next if dossier.mef_destination == cm2
 
   code_mef_destination = noeud_eleve.xpath("CODE_MEF").text
   mef_destination = Mef.find_by(etablissement: etablissement, code: code_mef_destination)
-  puts "mise à jour #{ine} de mef destination #{dossier.mef_desination_id} pour #{mef_destination.id}"
+  puts "mise à jour #{ine} de mef destination #{dossier.mef_destination_id} pour #{mef_destination.id}"
   dossier.update(mef_destination: mef_destination)
 
   code_mef_an_dernier = noeud_eleve.xpath("SCOLARITE_AN_DERNIER/CODE_MEF").text
   puts "mise à jour #{ine} de mef an dernier #{dossier.mef_an_dernier} pour #{code_mef_an_dernier}"
   dossier.update(mef_an_dernier: code_mef_an_dernier)
-
 end
