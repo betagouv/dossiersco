0e84d6300e97bf9e7c481d76a2a0a93fd809b9d4
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2019-09-19 18:03:56 +0200

    Affiche uniquement la liste des pieces jointes validees
    Co-authored-by: yaf <yannick.francois@beta.gouv.fr>

--- scripts/recupere_pieces_jointes_etablissement.rb
+++ scripts/recupere_pieces_jointes_etablissement.rb
@@ -10,10 +10,10 @@ liste = []
 
 Etablissement.find_by(uai: uai).dossier_eleve.each do |dossier|
   liste << {
-    ine: dossier.eleve.identifiant,
-    prenom: dossier.prenom,
-    nom: dossier.eleve.nom,
-    fichiers: piece_jointe.flatten.map(&:fichiers).flatten.map(&:path)
+    "ine" => dossier.eleve.identifiant,
+    "prenom" => dossier.eleve.prenom,
+    "nom" => dossier.eleve.nom,
+    "fichiers" => dossier.piece_jointe.select{|pj| pj.etat == PieceJointe::ETATS[:valide]}.map(&:fichiers).flatten.map(&:path)
   }
 end
 
