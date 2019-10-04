9f39414a4152bc6163a6ec3d7902dfd25b00a59d
Philippe Blayo <philippe.blayo@beta.gouv.fr>
2019-10-04 18:09:33 +0200

    détermine le genre du contact à partir du prénom
    Co-authored-by: krichtof <christophe.robillard@beta.gouv.fr>
    Co-authored-by: yaf <yannick.francois@beta.gouv.fr>

--- scripts/nettoyage_civilite_contact_en_cas_urgence.rb
+++ scripts/nettoyage_civilite_contact_en_cas_urgence.rb
@@ -1,38 +1,16 @@
+# frozen_string_literal: true
 
-PRENOMS_MASCULINS = <<EOF
-- daniel
-- olivier
-- bilal
-- laurent
-EOF
-
-PRENOMS_FEMININS = <<EOF
-- chantal
-- amina
-- angélique
-EOF
-
-def madame?(prenom)
-  YAML.load(PRENOMS_FEMININS).include?(prenom)
-end
-
-def monsieur?(prenom)
-  YAML.load(PRENOMS_MASCULINS).include?(prenom)
-end
-
-ContactUrgence.where(civilite: nil).sample(10).each do |contact_sans_civilite|
-  prenom = contact_sans_civilite.prenom&.downcase || ""
-
-  if monsieur?(prenom)
-    puts "#{prenom} : monsieur 1"
-    #contact_sans_civilite.update(civilite: 1)
-  elsif madame?(prenom)
-    puts "#{prenom} : madame 2"
-    #contact_sans_civilite.update(civilite: 2)
-  else
-    puts "prenom #{prenom} non trouvé"
+def genre(prenom)
+  [1,2].each do |genre|
+    return genre if `grep -i "#{genre};#{prenom}" lib/nat2018.csv`.present?
   end
 
+  nil
 end
 
+ContactUrgence.where(civilite: nil).each do |contact_sans_civilite|
+  prenom = contact_sans_civilite.prenom&.upcase || ""
 
+  puts genre(prenom)
+  contact_sans_civilite.update(civilite: genre(prenom))
+end
