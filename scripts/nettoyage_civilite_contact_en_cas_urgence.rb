8b626f3842c8a7d2f786c32485acf26564b9ad8b
Philippe Blayo <philippe.blayo@beta.gouv.fr>
2019-10-04 18:09:33 +0200

    détermine le genre du contact à partir du prénom
    Co-authored-by: krichtof <christophe.robillard@beta.gouv.fr>
    Co-authored-by: yaf <yannick.francois@beta.gouv.fr>

--- scripts/nettoyage_civilite_contact_en_cas_urgence.rb
+++ scripts/nettoyage_civilite_contact_en_cas_urgence.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 def genre(prenom)
-  [1,2].each do |genre|
-    return genre if `grep -i "#{genre};#{prenom}" lib/nat2018.csv`.present?
+  [1, 2].each do |genre|
+    return genre if `grep -i "#{genre};#{prenom}" #{Rails.root}/lib/prenoms.csv`.present?
   end
 
   nil
 end
 
 ContactUrgence.where(civilite: nil).each do |contact_sans_civilite|
-  prenom = contact_sans_civilite.prenom&.upcase || ""
+  prenom = contact_sans_civilite.prenom&.upcase&.strip || ""
 
   puts genre(prenom)
   contact_sans_civilite.update(civilite: genre(prenom))
