d5b9534e9785f92e475919b8d26334cc80888afe
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2019-10-08 10:27:08 +0200

    [refactor] supprime une règle rubocop

--- scripts/nettoyage_lien_de_parent_contact_urgence.rb
+++ scripts/nettoyage_lien_de_parent_contact_urgence.rb
@@ -18,164 +18,35 @@ def pere?(contact)
 end
 
 def fratrie?(contact)
-  contact.lien_de_parente == "fratrie" ||
-    contact.lien_de_parente == "frere" ||
-    contact.lien_de_parente == "frere aine" ||
-    contact.lien_de_parente == "frere aîne" ||
-    contact.lien_de_parente == "frere (aine)" ||
-    contact.lien_de_parente == "frere majeur" ||
-    contact.lien_de_parente == "grand frere" ||
-    contact.lien_de_parente == "demi-frère" ||
-    contact.lien_de_parente == "enfant" ||
-    contact.lien_de_parente == "frere (aine)" ||
-    contact.lien_de_parente == "frere / parrain civil" ||
-    contact.lien_de_parente == "grand soeur" ||
-    contact.lien_de_parente == "grand frere" ||
-    contact.lien_de_parente == "grande soeur 1" ||
-    contact.lien_de_parente == "grande sœur" ||
-    contact.lien_de_parente == "sa soeur" ||
-    contact.lien_de_parente == "le frere" ||
-    contact.lien_de_parente == "soeur" ||
-    contact.lien_de_parente == "sœur (aînee)" ||
-    contact.lien_de_parente == "sœur aînee" ||
-    contact.lien_de_parente == "sœurs" ||
-    contact.lien_de_parente == "seur" ||
-    contact.lien_de_parente == "fille" ||
-    contact.lien_de_parente == "fils" ||
-    contact.lien_de_parente == "soeur (majeure)" ||
-    contact.lien_de_parente == "soeur ainee" ||
-    contact.lien_de_parente == "soeurs" ||
-    contact.lien_de_parente == "grande soeur" ||
-    contact.lien_de_parente == "sœur"
+  ["fratrie", "frere", "frere aine", "frere aîne", "frere (aine)", "frere majeur", "grand frere", "demi-frère",
+   "enfant", "frere (aine)", "frere / parrain civil", "grand soeur", "grand frere", "grande soeur 1", "grande sœur",
+   "sa soeur", "le frere", "soeur", "sœur (aînee)", "sœur aînee", "sœurs", "seur", "fille", "fils", "soeur (majeure)",
+   "soeur ainee", "soeurs", "grande soeur", "sœur"].include?(contact.lien_de_parente)
 end
 
 def ascendant?(contact)
-  contact.lien_de_parente == "ascendant" ||
-    contact.lien_de_parente == "grnad mere" ||
-    contact.lien_de_parente == "grand pere" ||
-    contact.lien_de_parente == "g parents" ||
-    contact.lien_de_parente == "gd mere" ||
-    contact.lien_de_parente == "gran mere" ||
-    contact.lien_de_parente == "grand   mere" ||
-    contact.lien_de_parente == "grand  mere" ||
-    contact.lien_de_parente == "grand meere" ||
-    contact.lien_de_parente == "grand mere maternelles" ||
-    contact.lien_de_parente == "grand mere sebahi eliane" ||
-    contact.lien_de_parente == "grand parents maternels" ||
-    contact.lien_de_parente == "grand partent" ||
-    contact.lien_de_parente == "grand pere et grand mere" ||
-    contact.lien_de_parente == "grand pere et medecin" ||
-    contact.lien_de_parente == "grand_mere" ||
-    contact.lien_de_parente == "grande mere" ||
-    contact.lien_de_parente == "grande pere" ||
-    contact.lien_de_parente == "grandmere" ||
-    contact.lien_de_parente == "grands  parents" ||
-    contact.lien_de_parente == "grands parentd" ||
-    contact.lien_de_parente == "grands parents maternelle s" ||
-    contact.lien_de_parente == "grands pere" ||
-    contact.lien_de_parente == "grang mere" ||
-    contact.lien_de_parente == "granp pere" ||
-    contact.lien_de_parente == "grend mere" ||
-    contact.lien_de_parente == "mamy" ||
-    contact.lien_de_parente == "papy" ||
-    contact.lien_de_parente == "pere mamie" ||
-    contact.lien_de_parente == "sa mamie" ||
-    contact.lien_de_parente == "grand  pere" ||
-    contact.lien_de_parente == "grand pere paternel" ||
-    contact.lien_de_parente == "grand pere maternel" ||
-    contact.lien_de_parente == "grand mere" ||
-    contact.lien_de_parente == "grand  mere" ||
-    contact.lien_de_parente == "grands parentd" ||
-    contact.lien_de_parente == "grand mere paternel" ||
-    contact.lien_de_parente == "grand mere paternelle" ||
-    contact.lien_de_parente == "grand mere maternel" ||
-    contact.lien_de_parente == "grand mere,soeur" ||
-    contact.lien_de_parente == "grand mere maternelle" ||
-    contact.lien_de_parente == "gd mere maternelle" ||
-    contact.lien_de_parente == "grand parent" ||
-    contact.lien_de_parente == "grands parents" ||
-    contact.lien_de_parente == "grand parents" ||
-    contact.lien_de_parente == "grands parents maternels" ||
-    contact.lien_de_parente == "grands parents paternels" ||
-    contact.lien_de_parente == "mamie maternelle" ||
-    contact.lien_de_parente == "mamie" ||
-    contact.lien_de_parente == "mami" ||
-    contact.lien_de_parente == "papi" ||
-    contact.lien_de_parente == "compagne du pere" ||
-    contact.lien_de_parente == "grand pere et médecin"
+  [
+    "ascendant", "grnad mere", "grand pere", "g parents", "gd mere", "gran mere", "grand   mere", "grand  mere", "grand meere",
+    "grand mere maternelles", "grand mere sebahi eliane", "grand parents maternels", "grand partent", "grand pere et grand mere",
+    "grand pere et medecin", "grand_mere", "grande mere", "grande pere", "grandmere", "grands  parents", "grands parentd",
+    "grands parents maternelle s", "grands pere", "grang mere", "granp pere", "grend mere", "mamy", "papy", "pere mamie", "sa mamie",
+    "grand  pere", "grand pere paternel", "grand pere maternel", "grand mere", "grand  mere",
+    "grands parentd", "grand mere paternel", "grand mere paternelle", "grand mere maternel", "grand mere,soeur",
+    "grand mere maternelle", "gd mere maternelle", "grand parent", "grands parents", "grand parents", "grands parents maternels",
+    "grands parents paternels", "mamie maternelle", "mamie", "mami", "papi", "compagne du pere", "grand pere et médecin"
+  ].include?(contact.lien_de_parente)
 end
 
 def autre_membre?(contact)
-  contact.lien_de_parente == "autremembredelafamille" ||
-    contact.lien_de_parente == "autre membre de la famille" ||
-    contact.lien_de_parente == "grands parents de coeur" ||
-    contact.lien_de_parente == "beau pere" ||
-    contact.lien_de_parente == "beau  pere" ||
-    contact.lien_de_parente == "beau   pere" ||
-    contact.lien_de_parente == "beau papa" ||
-    contact.lien_de_parente == "beau parent" ||
-    contact.lien_de_parente == "beau peere" ||
-    contact.lien_de_parente == "beau_pere" ||
-    contact.lien_de_parente == "beaux pere" ||
-    contact.lien_de_parente == "belle maman" ||
-    contact.lien_de_parente == "belle mere / marraine" ||
-    contact.lien_de_parente == "marie" ||
-    contact.lien_de_parente == "bo pere" ||
-    contact.lien_de_parente == "beau papa" ||
-    contact.lien_de_parente == "compagnon de la mere" ||
-    contact.lien_de_parente == "concubin" ||
-    contact.lien_de_parente == "compagnon de la mere" ||
-    contact.lien_de_parente == "epoux de la mere" ||
-    contact.lien_de_parente == "belle mere" ||
-    contact.lien_de_parente == "conjointe du pere" ||
-    contact.lien_de_parente == "conjoint de la mere" ||
-    contact.lien_de_parente == "compagnon mere" ||
-    contact.lien_de_parente == "tante" ||
-    contact.lien_de_parente == "parente" ||
-    contact.lien_de_parente == "parents" ||
-    contact.lien_de_parente == "oncle / tante" ||
-    contact.lien_de_parente == "oncle et tante" ||
-    contact.lien_de_parente == "oncle maternel" ||
-    contact.lien_de_parente == "oncle paternel" ||
-    contact.lien_de_parente == "oncle/ tante" ||
-    contact.lien_de_parente == "neveu" ||
-    contact.lien_de_parente == "niece" ||
-    contact.lien_de_parente == "marainne" ||
-    contact.lien_de_parente == "la tanre" ||
-    contact.lien_de_parente == "l oncle" ||
-    contact.lien_de_parente == "grande cousine" ||
-    contact.lien_de_parente == "grand oncle" ||
-    contact.lien_de_parente == "sa tante" ||
-    contact.lien_de_parente == "sa tante maternelle" ||
-    contact.lien_de_parente == "sa tatiee" ||
-    contact.lien_de_parente == "son oncle" ||
-    contact.lien_de_parente == "compagnon mere" ||
-    contact.lien_de_parente == "conjoint de la mere" ||
-    contact.lien_de_parente == "cousin" ||
-    contact.lien_de_parente == "cousine et voisine" ||
-    contact.lien_de_parente == "epoux de la mere" ||
-    contact.lien_de_parente == "demi frere" ||
-    contact.lien_de_parente == "demi soeur" ||
-    contact.lien_de_parente == "tant" ||
-    contact.lien_de_parente == "tante et maraine" ||
-    contact.lien_de_parente == "tante maternelle" ||
-    contact.lien_de_parente == "tante paternelle" ||
-    contact.lien_de_parente == "tata et tonon" ||
-    contact.lien_de_parente == "tati" ||
-    contact.lien_de_parente == "tente" ||
-    contact.lien_de_parente == "tata" ||
-    contact.lien_de_parente == "tatie" ||
-    contact.lien_de_parente == "la tante" ||
-    contact.lien_de_parente == "la tente" ||
-    contact.lien_de_parente == "grande tante" ||
-    contact.lien_de_parente == "oncle" ||
-    contact.lien_de_parente == "tonton" ||
-    contact.lien_de_parente == "ma niece" ||
-    contact.lien_de_parente == "cousine" ||
-    contact.lien_de_parente == "famille" ||
-    contact.lien_de_parente == "marraine" ||
-    contact.lien_de_parente == "maraine" ||
-    contact.lien_de_parente == "parrain"
+  ["autremembredelafamille", "autre membre de la famille", "grands parents de coeur", "beau pere", "beau  pere",
+   "beau   pere", "beau papa", "beau parent", "beau peere", "beau_pere", "beaux pere", "belle maman", "belle mere / marraine",
+   "marie", "bo pere", "beau papa", "compagnon de la mere", "concubin", "compagnon de la mere", "epoux de la mere", "belle mere",
+   "conjointe du pere", "conjoint de la mere", "compagnon mere", "tante", "parente", "parents", "oncle / tante",
+   "oncle et tante", "oncle maternel", "oncle paternel", "oncle/ tante", "neveu", "niece", "marainne", "la tanre", "l oncle",
+   "grande cousine", "grand oncle", "sa tante", "sa tante maternelle", "sa tatiee", "son oncle", "compagnon mere", "conjoint de la mere",
+   "cousin", "cousine et voisine", "epoux de la mere", "demi frere", "demi soeur", "tant", "tante et maraine",
+   "tante maternelle", "tante paternelle", "tata et tonon", "tati", "tente", "tata", "tatie", "la tante", "la tente",
+   "grande tante", "oncle", "tonton", "ma niece", "cousine", "famille", "marraine", "maraine", "parrain"].include?(contact.lien_de_parente)
 end
 
 def educateur?(contact)
@@ -185,18 +56,10 @@ def educateur?(contact)
 end
 
 def assistante_familliale?(contact)
-  contact.lien_de_parente == "ass.famil" ||
-    contact.lien_de_parente == "ass  famil" ||
-    contact.lien_de_parente == "assistante maternelle" ||
-    contact.lien_de_parente == "nounou" ||
-    contact.lien_de_parente == "garde domicile" ||
-    contact.lien_de_parente == "nounou de son frere" ||
-    contact.lien_de_parente == "nourrice" ||
-    contact.lien_de_parente == "nourrrice" ||
-    contact.lien_de_parente == "assistante familiale" ||
-    contact.lien_de_parente == "assistant familial" ||
-    contact.lien_de_parente == "assistante familial" ||
-    contact.lien_de_parente == "assistante maternelle de la famille"
+  [
+    "ass.famil", "ass  famil", "assistante maternelle", "nounou", "garde domicile", "nounou de son frere", "nourrice",
+    "nourrrice", "assistante familiale", "assistant familial", "assistante familial", "assistante maternelle de la famille"
+  ].include?(contact.lien_de_parente)
 end
 
 def tuteur?(contact)
