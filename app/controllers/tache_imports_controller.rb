358390336ec21a90aed2f9a23db1ac31af0bc3a7
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2019-10-09 18:36:49 +0200

    [refactor] supprime une exception rubocop

--- app/controllers/tache_imports_controller.rb
+++ app/controllers/tache_imports_controller.rb
@@ -6,32 +6,21 @@ class TacheImportsController < ApplicationController
   layout "configuration"
 
   def new
-    @tache = agent_connecte.etablissement.tache_import.last ||
-             TacheImport.new(etablissement: agent_connecte.etablissement)
-    @fichiers_changements_a_telecharger = FichierATelecharger
-                                          .where(nom: "changements", etablissement: agent_connecte.etablissement)
-                                          .order("created_at DESC")
-    @fichiers_a_telecharger = FichierATelecharger
-                              .where(nom: "eleves", etablissement: agent_connecte.etablissement)
-                              .order("created_at DESC")
-    @pieces_a_telecharger = FichierATelecharger
-                            .where(nom: "pieces-jointes", etablissement: agent_connecte.etablissement)
-                            .order("created_at DESC")
+    @tache = agent_connecte.etablissement.tache_import.last || TacheImport.new(etablissement: agent_connecte.etablissement)
+    @fichiers_changements_a_telecharger = FichierATelecharger.de_type_changement(agent_connecte.etablissement)
+    @fichiers_a_telecharger = FichierATelecharger.de_type_eleve(agent_connecte.etablissement)
+    @pieces_a_telecharger = FichierATelecharger.de_type_piece(agent_connecte.etablissement)
     @mef = agent_connecte.etablissement.mef
   end
 
   def create
-    tache = TacheImport.find_by(etablissement: agent_connecte.etablissement,
-                                statut: [TacheImport::STATUTS[:en_attente],
-                                         TacheImport::STATUTS[:en_traitement]])
+    tache = TacheImport .find_by(etablissement: agent_connecte.etablissement,
+                                 statut: [TacheImport::STATUTS[:en_attente], TacheImport::STATUTS[:en_traitement]])
 
     if tache.present?
       flash[:alert] = t(".tache_deja_en_traitement")
     elsif params[:tache_import].present?
-      tache_import = TacheImport.create(tache_import_params.merge(etablissement: agent_connecte.etablissement,
-                                                                  statut: TacheImport::STATUTS[:en_attente]))
-      ImporterSiecle.perform_later(tache_import.id, agent_connecte.email)
-      flash[:notice] = t(".message_de_succes", email: agent_connecte.email)
+      importer_dans_siecle
     else
       flash[:alert] = t(".fichier_manquant")
     end
@@ -40,6 +29,13 @@ class TacheImportsController < ApplicationController
   end
 
   private
+
+  def importer_dans_siecle
+    tache_import = TacheImport.create(tache_import_params.merge(etablissement: agent_connecte.etablissement,
+                                                                statut: TacheImport::STATUTS[:en_attente]))
+    ImporterSiecle.perform_later(tache_import.id, agent_connecte.email)
+    flash[:notice] = t(".message_de_succes", email: agent_connecte.email)
+  end
 
   def tache_import_params
     params.require(:tache_import).permit(:fichier, :type_fichier)
--- app/models/fichier_a_telecharger.rb
+++ app/models/fichier_a_telecharger.rb
@@ -6,4 +6,19 @@ class FichierATelecharger < ApplicationRecord
 
   mount_uploader :contenu, FichierATelechargerUploader
 
+  scope :de_type_changement, lambda { |etablissement|
+    where(nom: "changements", etablissement: etablissement)
+      .order("created_at DESC")
+  }
+
+  scope :de_type_eleve, lambda { |etablissement|
+    where(nom: "eleves", etablissement: etablissement)
+      .order("created_at DESC")
+  }
+
+  scope :de_type_piece, lambda { |etablissement|
+    where(nom: "pieces-jointes", etablissement: etablissement)
+      .order("created_at DESC")
+  }
+
 end
