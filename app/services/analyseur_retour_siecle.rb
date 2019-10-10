814e6b6228dffae13e9c922170839b6dd6d37375
Christophe Robillard <christophe.robillard@beta.gouv.fr>
2019-10-10 12:55:56 +0200

    [refactor] supprime une exception rubocop

--- app/services/analyseur_retour_siecle.rb
+++ app/services/analyseur_retour_siecle.rb
@@ -3,14 +3,15 @@
 class AnalyseurRetourSiecle
 
   def self.analyse(dossier)
-    return I18n.t("retour_siecles.dossier_non_valide") unless dossier.valide?
-    return I18n.t("retour_siecles.dossier_sans_mef_destination") unless dossier.mef_destination.present?
-    return I18n.t("retour_siecles.dossier_sans_mef_an_dernier") unless dossier.mef_an_dernier.present?
-    return I18n.t("retour_siecles.dossier_sans_nom_ou_prenom") unless dossier.prenom.present?
-    return I18n.t("retour_siecles.dossier_sans_nom_ou_prenom") unless dossier.nom.present?
-    return I18n.t("retour_siecles.probleme_commune_naissance") unless commune_insee_renseigner_si_nee_en_france?(dossier)
-    return I18n.t("retour_siecles.dossier_avec_mef_origine_invalide") unless dossier.mef_origine.code.length == 11
-    return I18n.t("retour_siecles.dossier_avec_mef_destination_invalide") unless dossier.mef_destination.code.length == 11
+    {
+      "retour_siecles.dossier_non_valide" => -> { dossier.valide? },
+      "retour_siecles.dossier_sans_mef_destination" => -> { dossier.mef_destination.present? },
+      "retour_siecles.dossier_sans_mef_an_dernier" => -> { dossier.mef_an_dernier.present? },
+      "retour_siecles.dossier_sans_nom_ou_prenom" => -> { dossier.prenom.present? && dossier.nom.present? },
+      "retour_siecles.probleme_commune_naissance" => -> { commune_insee_renseigner_si_nee_en_france?(dossier) },
+      "retour_siecles.dossier_avec_mef_origine_invalide" => -> { dossier.mef_origine.code.length == 11 },
+      "retour_siecles.dossier_avec_mef_destination_invalide" => -> { dossier.mef_destination.code.length == 11 }
+    }.each { |probleme, condition| return I18n.t(probleme) unless condition.call }
 
     ""
   end
