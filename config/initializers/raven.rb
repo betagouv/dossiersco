950f3c22bab07e83c61133ad07b1b4d36ad2eca2
Yannick Francois <yannick.francois@beta.gouv.fr>
2019-12-02 10:48:18 +0100

    Supprime sentry

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,6 +1,2 @@
 # frozen_string_literal: true
 
-Raven.configure do |config|
-  config.dsn = ENV["SENTRY_DSN"]
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-end
